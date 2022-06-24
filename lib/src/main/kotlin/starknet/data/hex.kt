2595fca59cff5d9bec397033c3fa5ee636c669b6
Jakub Ptak <jakub.ptak@swmansion.com>
2022-06-24 08:45:09 +0200

    Add uint256

---
+++ lib/src/main/kotlin/starknet/data/hex.kt
@@ -0,0 +1,14 @@
+@file:JvmName("Hex")
+
+package starknet.data
+
+import java.math.BigInteger
+
+fun parseHex(value: String): BigInteger {
+    if (!value.startsWith("0x")) {
+        throw IllegalArgumentException("Hex must start with 0x")
+    }
+    return BigInteger(value.removePrefix("0x"), 16)
+}
+
+fun toHex(value: BigInteger): String = "0x" + value.toString(16)
