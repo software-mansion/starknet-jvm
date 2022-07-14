f1e98c35fa66bc6f5069bb245cf9de87b882c648
Artur Michalek <artur.michalek@swmansion.com>
2022-07-14 11:26:51 +0200

    Add apply false to dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,6 @@ plugins {
     id("com.android.application") version "7.2.1" apply false
     id("org.jetbrains.kotlin.android") version "1.6.10" apply false
     id("org.jetbrains.kotlin.jvm") version "1.7.0" apply false
-    id("org.jetbrains.dokka") version "1.7.0"
+    id("org.jetbrains.dokka") version "1.7.0" apply false
     kotlin("plugin.serialization") version "1.7.0" apply false
 }
