cb62ddff996beaa5ee7c693019ea5f733effcfad
Jakub Ptak <jakub.ptak@swmansion.com>
2022-07-18 10:39:00 +0200

    Bump kotlin.android

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 plugins {
     id("com.android.library") version "7.0.4" apply false
     id("com.android.application") version "7.0.4" apply false
-    id("org.jetbrains.kotlin.android") version "1.6.10" apply false
+    id("org.jetbrains.kotlin.android") version "1.7.0" apply false
     id("org.jetbrains.kotlin.jvm") version "1.7.0" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "1.7.0"
     id("org.jetbrains.dokka") version "1.7.0" apply false
