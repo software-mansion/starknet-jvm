d7446ee5137336f31e634584daf1f80d714e55df
Jakub Ptak <jakub.ptak@swmansion.com>
2022-07-18 11:26:11 +0200

    Copy any shared lib

--- lib/build_crypto_cpp.sh
+++ lib/build_crypto_cpp.sh
@@ -2,4 +2,4 @@
 
 mkdir -p build/libs/shared
 (cd ../crypto && ./build.sh) || exit
-cp -f ../crypto/build/bindings/libcrypto_jni.dylib build/libs/shared
+cp -f ../crypto/build/bindings/libcrypto_jni.* build/libs/shared
