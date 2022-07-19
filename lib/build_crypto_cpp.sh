b526576a724536abc55d905448033705484a2291
Jakub Ptak <jakub.ptak@swmansion.com>
2022-07-19 13:15:57 +0200

    Add universal jar

--- lib/build_crypto_cpp.sh
+++ lib/build_crypto_cpp.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
+set -e
 
 mkdir -p build/libs/shared
 (cd ../crypto && ./build.sh) || exit
-cp -f ../crypto/build/bindings/libcrypto_jni.* build/libs/shared
+
+# Move libraries to a proper directory
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  ARCH=$(arch)
+  SUFFIX="linux/$ARCH"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  # Darwin uses a universal binary, no arch is needed
+  SUFFIX="darwin"
+else
+  echo "OS not supported at the moment"
+  exit 1;
+fi
+
+mkdir -p build/libs/shared/$SUFFIX
+cp -f ../crypto/build/bindings/libcrypto_jni.* build/libs/shared/$SUFFIX
