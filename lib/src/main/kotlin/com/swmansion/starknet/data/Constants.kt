9758f2faf580e671ca93918204f1985de2372dd9
Artur Michalek <artur.michalek@swmansion.com>
2022-08-04 15:03:51 +0200

    Make constants const

--- lib/src/main/kotlin/com/swmansion/starknet/data/Constants.kt
+++ lib/src/main/kotlin/com/swmansion/starknet/data/Constants.kt
@@ -2,10 +2,10 @@ package com.swmansion.starknet.data
 
 import com.swmansion.starknet.data.types.Felt
 
-val DEFAULT_ENTRY_POINT_NAME = "__default__"
-val DEFAULT_L1_ENTRY_POINT_NAME = "__l1_default__"
-val DEFAULT_ENTRY_POINT_SELECTOR = 0
-val EXECUTE_ENTRY_POINT_NAME = "__execute__"
-val TRANSFER_ENTRY_POINT_NAME = "transfer"
+const val DEFAULT_ENTRY_POINT_NAME = "__default__"
+const val DEFAULT_L1_ENTRY_POINT_NAME = "__l1_default__"
+const val DEFAULT_ENTRY_POINT_SELECTOR = 0
+const val EXECUTE_ENTRY_POINT_NAME = "__execute__"
+const val TRANSFER_ENTRY_POINT_NAME = "transfer"
 
 val DECLARE_SENDER_ADDRESS = Felt(1)
