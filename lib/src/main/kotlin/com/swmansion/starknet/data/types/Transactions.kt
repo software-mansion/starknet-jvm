919535715a87818ace7fc0f1d63e385ee302e580
Artur Michalek <artur.michalek@swmansion.com>
2022-08-04 15:04:24 +0200

    Remove empty default constructors

--- lib/src/main/kotlin/com/swmansion/starknet/data/types/Transactions.kt
+++ lib/src/main/kotlin/com/swmansion/starknet/data/types/Transactions.kt
@@ -36,7 +36,7 @@ enum class BlockTag(val tag: String) {
 }
 
 @Serializable(with = BlockHashOrTagSerializer::class)
-sealed class BlockHashOrTag() {
+sealed class BlockHashOrTag {
     data class Hash(
         val blockHash: Felt,
     ) : BlockHashOrTag() {
@@ -56,7 +56,7 @@ sealed class BlockHashOrTag() {
     abstract fun string(): String
 }
 
-class BlockHashOrTagSerializer() : KSerializer<BlockHashOrTag> {
+class BlockHashOrTagSerializer : KSerializer<BlockHashOrTag> {
     override fun deserialize(decoder: Decoder): BlockHashOrTag {
         val value = decoder.decodeString()
 
