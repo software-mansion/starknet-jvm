32efdf419b33fdf26c084b16755a0b170f279415
Artur Michalek <artur.michalek@swmansion.com>
2022-08-05 12:30:03 +0200

    Move constants to selector

--- lib/src/main/kotlin/com/swmansion/starknet/data/Constants.kt
+++ lib/src/main/kotlin/com/swmansion/starknet/data/Constants.kt
@@ -4,10 +4,4 @@ package com.swmansion.starknet.data
 
 import com.swmansion.starknet.data.types.Felt
 
-const val DEFAULT_ENTRY_POINT_NAME = "__default__"
-const val DEFAULT_L1_ENTRY_POINT_NAME = "__l1_default__"
-const val DEFAULT_ENTRY_POINT_SELECTOR = 0
-const val EXECUTE_ENTRY_POINT_NAME = "__execute__"
-const val TRANSFER_ENTRY_POINT_NAME = "transfer"
-
 val DECLARE_SENDER_ADDRESS = Felt(1)
