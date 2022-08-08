a20f9819dfda2cca8f3f4385758af91314a43148
Bartosz Rybarski <bartosz.rybarski@swmansion.com>
2022-08-08 13:04:44 +0200

    Lint

--- lib/src/main/kotlin/com/swmansion/starknet/data/types/Transactions.kt
+++ lib/src/main/kotlin/com/swmansion/starknet/data/types/Transactions.kt
@@ -2,14 +2,4 @@
 
 package com.swmansion.starknet.data.types
 
-import kotlinx.serialization.KSerializer
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.descriptors.PrimitiveKind
-import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
-import kotlinx.serialization.descriptors.SerialDescriptor
-import kotlinx.serialization.encoding.Decoder
-import kotlinx.serialization.encoding.Encoder
 import kotlinx.serialization.json.*
-
-
