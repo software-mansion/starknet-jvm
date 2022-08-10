d0cd3d85e936f209c37ff93db7711a29497ec7a1
Jakub Ptak <jakub.ptak@swmansion.com>
2022-08-10 16:52:54 +0200

    Use base64 from bouncy castle

--- lib/src/main/kotlin/com/swmansion/starknet/extensions/Base64Gzipped.kt
+++ lib/src/main/kotlin/com/swmansion/starknet/extensions/Base64Gzipped.kt
@@ -1,12 +1,13 @@
 package com.swmansion.starknet.extensions
 
+import org.bouncycastle.util.encoders.Base64
 import java.io.ByteArrayOutputStream
 import java.nio.charset.StandardCharsets
-import java.util.*
 import java.util.zip.GZIPOutputStream
 
 fun String.base64Gzipped(): String {
     val bos = ByteArrayOutputStream()
     GZIPOutputStream(bos).bufferedWriter(StandardCharsets.UTF_8).use { it.write(this) }
-    return Base64.getEncoder().encodeToString(bos.toByteArray())
+    val base64Encoded = ByteArrayOutputStream().use { Base64.encode(bos.toByteArray(), it) }
+    return base64Encoded.toString()
 }
