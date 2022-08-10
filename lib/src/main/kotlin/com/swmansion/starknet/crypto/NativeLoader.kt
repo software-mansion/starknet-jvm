e272946ae16ed319a88da32c36ee6a07228e9da4
Jakub Ptak <solpatium003@gmail.com>
2022-08-10 11:50:35 +0200

    Add build job (#29)
    * Add example job
    
    * Add changes to build on linux & mac
    
    * Fix workflow
    
    * Add distribution
    
    * Upgrade java version
    
    * Fetch crypto-cpp
    
    * Build on mac only
    
    * Remove gradle cache
    
    * Allow gradle deamon
    
    * Bump kotlin.android
    
    * Build lib alone
    
    * Build lib alone
    
    * Build on ubuntu
    
    * Copy any shared lib
    
    * Enable android build
    
    * Enable build for both linux & ubuntu
    
    * Improve build workflow
    
    * Fix android build
    
    * Build universal binary for mac
    
    * Add universal jar
    
    * Fix yml
    
    * Fix build
    
    * Fix build
    
    * Fix mkdir
    
    * Add globs for cp
    
    * Add maven publication draft code
    
    * Rename workflows
    
    * Move jar packaging
    
    * Modify for maven publish
    
    * Rename project + modify publish flow
    
    * Publish
    
    * Add strict dependency to POM
    
    * Add flow for different archs
    
    * Install deps inside container
    
    * Publish
    
    * Cleanup + add dependency to javademo
    
    * Fix linting
    
    * Add multiarch flow (#67)
    
    * Add multiarch flow
    
    * Combine steps
    
    * debug
    
    * Use action for build
    
    * use proper driver
    
    * build
    
    * Increase metaspace size
    
    * Bump api version
    
    * Build
    
    * cleanup
    
    * review changes
    
    Co-authored-by: Wojciech Szymczyk <wojciech.szymczyk@swmansion.com>

--- lib/src/main/kotlin/com/swmansion/starknet/crypto/NativeLoader.kt
+++ lib/src/main/kotlin/com/swmansion/starknet/crypto/NativeLoader.kt
@@ -1,30 +1,67 @@
 package com.swmansion.starknet.crypto
 
+import java.net.URL
 import java.nio.file.FileSystems
 import java.nio.file.Files
+import java.util.*
 
 internal object NativeLoader {
-    private val sharedLibExtensions = listOf("dylib", "so", "dll")
+    fun load(name: String) = load(name, operatingSystem, architecture)
 
-    fun load(name: String) {
+    internal fun load(name: String, operatingSystem: SystemType, architecture: String) {
         try {
-            // Used for tests and in case someone wants to use a library from
+            // Used for tests, on android and in case someone wants to use a library from
             // a class path.
             System.loadLibrary(name)
         } catch (e: UnsatisfiedLinkError) {
             // Find the package bundled in this jar
-            val resource = sharedLibExtensions.map {
-                val fileName = "/lib$name.$it"
-                object {}::class.java.getResource(fileName)
-            }.firstOrNull { it != null } ?: throw e
+            val path = getLibPath(operatingSystem, architecture, "lib" + name)
+            val resource =
+                NativeLoader::class.java.getResource(path) ?: throw UnsupportedPlatform(
+                    operatingSystem.name,
+                    architecture,
+                )
+            loadFromJar(name, resource)
+        }
+    }
 
-            // Android lint complains about it, but android should never reach this code
-            val tmpDir = Files.createTempDirectory("$name-dir").toFile().apply {
-                deleteOnExit()
-            }
-            val tmpFilePath = FileSystems.getDefault().getPath(tmpDir.absolutePath, name)
-            resource.openStream().use { Files.copy(it, tmpFilePath) }
-            System.load(tmpFilePath.toString())
+    @Suppress("UnsafeDynamicallyLoadedCode")
+    private fun loadFromJar(name: String, resource: URL) {
+        // Android lint complains about it, but android should never reach this code
+        val tmpDir = Files.createTempDirectory("$name-dir").toFile().apply {
+            deleteOnExit()
+        }
+        val tmpFilePath = FileSystems.getDefault().getPath(tmpDir.absolutePath, name)
+        resource.openStream().use { Files.copy(it, tmpFilePath) }
+        System.load(tmpFilePath.toString())
+    }
+
+    internal enum class SystemType {
+        Windows, MacOS, Linux, Other
+    }
+
+    private val operatingSystem: SystemType by lazy {
+        val system = System.getProperty("os.name", "generic").lowercase(Locale.ENGLISH)
+        when {
+            system.contains("mac") || system.contains("darwin") -> SystemType.MacOS
+            system.contains("win") -> SystemType.Windows
+            system.contains("nux") -> SystemType.Linux
+            else -> SystemType.Other
+        }
+    }
+
+    private val architecture: String by lazy {
+        System.getProperty("os.arch")
+    }
+
+    class UnsupportedPlatform(system: String, architecture: String) :
+        Exception("Unsupported platfrom $system:$architecture")
+
+    private fun getLibPath(system: SystemType, architecture: String, name: String): String {
+        return when (system) {
+            SystemType.MacOS -> "/darwin/$name.dylib"
+            SystemType.Linux -> "/linux/$architecture/$name.so"
+            else -> throw UnsupportedPlatform(operatingSystem.name, architecture)
         }
     }
 }
