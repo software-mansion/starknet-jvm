563c5689f991e723d22f4f0e3fb8c4843d6f1ded
Wojciech Szymczyk <wojciech.szymczyk@swmansion.com>
2022-08-12 16:37:01 +0200

    Release 0.0.3

--- androiddemo/build.gradle.kts
+++ androiddemo/build.gradle.kts
@@ -39,7 +39,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.4.2")
     implementation("com.google.android.material:material:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.swmansion.starknet:starknet:0.0.2")
+    implementation("com.swmansion.starknet:starknet:0.0.3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
