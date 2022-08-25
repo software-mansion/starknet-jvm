849afd561e732b4a515361172cfa231943c75ee2
Bartosz Rybarski <bartosz.rybarski@swmansion.com>
2022-08-25 11:01:42 +0200

    Add provider docs comments

--- lib/src/main/kotlin/com/swmansion/starknet/provider/Provider.kt
+++ lib/src/main/kotlin/com/swmansion/starknet/provider/Provider.kt
@@ -145,9 +145,33 @@ interface Provider {
      */
     fun declareContract(payload: DeclareTransactionPayload): Request<DeclareResponse>
 
+    /**
+     * estimate a fee.
+     *
+     * estimate a fee for a provided transaction.
+     *
+     * @param request invoke transaction, for which the fee is to be estimated.
+     * @param blockHash a hash of the block in respect to what the query will be made
+     */
     fun getEstimateFee(request: InvokeTransaction, blockHash: Felt): Request<EstimateFeeResponse>
 
+    /**
+     * estimate a fee.
+     *
+     * estimate a fee for a provided transaction.
+     *
+     * @param request invoke transaction, for which the fee is to be estimated.
+     * @param blockNumber a number of the block in respect to what the query will be made
+     */
     fun getEstimateFee(request: InvokeTransaction, blockNumber: Int): Request<EstimateFeeResponse>
 
+    /**
+     * estimate a fee.
+     *
+     * estimate a fee for a provided transaction.
+     *
+     * @param request invoke transaction, for which the fee is to be estimated.
+     * @param blockTag a tag of the block in respect to what the query will be made
+     */
     fun getEstimateFee(request: InvokeTransaction, blockTag: BlockTag): Request<EstimateFeeResponse>
 }
