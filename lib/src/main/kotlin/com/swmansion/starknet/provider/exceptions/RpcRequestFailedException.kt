6ec0ab46b139a1af76e02a9083c97d27c4686c59
Artur Michalek <artur.michalek@swmansion.com>
2022-09-07 16:03:33 +0200

    Document custom exceptions

--- lib/src/main/kotlin/com/swmansion/starknet/provider/exceptions/RpcRequestFailedException.kt
+++ lib/src/main/kotlin/com/swmansion/starknet/provider/exceptions/RpcRequestFailedException.kt
@@ -1,4 +1,11 @@
 package com.swmansion.starknet.provider.exceptions
 
+/**
+ * Exception thrown by rpc provider on request failure.
+ *
+ * @param code error code returned by the rpc provider
+ * @param message error message returned by the rpc provider
+ * @param payload payload returned by the service used to communicate with StarkNet
+ */
 class RpcRequestFailedException(val code: Int, message: String, payload: String) :
     RequestFailedException(message = message, payload = payload)
