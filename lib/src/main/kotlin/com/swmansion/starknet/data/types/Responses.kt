15f73204692849210dbaf986b90c9a6c529dc11c
Artur Michalek <artur.michalek@swmansion.com>
2022-09-12 11:50:39 +0200

    Remove moved serializer

--- lib/src/main/kotlin/com/swmansion/starknet/data/types/Responses.kt
+++ lib/src/main/kotlin/com/swmansion/starknet/data/types/Responses.kt
@@ -1,11 +1,9 @@
 package com.swmansion.starknet.data.types
 
-import com.swmansion.starknet.extensions.toFelt
-import kotlinx.serialization.*
-import kotlinx.serialization.descriptors.SerialDescriptor
-import kotlinx.serialization.encoding.Decoder
-import kotlinx.serialization.encoding.Encoder
-import kotlinx.serialization.json.*
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonNames
 
 @Serializable
 data class CallContractResponse(
@@ -61,37 +59,3 @@ data class EstimateFeeResponse(
     val overallFee: Felt,
 )
 
-@OptIn(ExperimentalSerializationApi::class)
-@Serializer(forClass = EstimateFeeResponse::class)
-class EstimateFeeResponseGatewaySerializer : KSerializer<EstimateFeeResponse> {
-    override fun deserialize(decoder: Decoder): EstimateFeeResponse {
-        val input = decoder as? JsonDecoder ?: throw SerializationException("Expected JsonInput for ${decoder::class}")
-
-        val jsonObject = input.decodeJsonElement().jsonObject
-
-        val gasUsage = jsonObject.getValue("gas_usage").jsonPrimitive.content.toBigInteger().toFelt
-        val gasPrice = jsonObject.getValue("gas_price").jsonPrimitive.content.toBigInteger().toFelt
-        val overallFee = jsonObject.getValue("overall_fee").jsonPrimitive.content.toBigInteger().toFelt
-
-        return EstimateFeeResponse(
-            gasConsumed = gasUsage,
-            gasPrice = gasPrice,
-            overallFee = overallFee,
-        )
-    }
-
-    override val descriptor: SerialDescriptor
-        get() = EstimateFeeResponse.serializer().descriptor
-
-    override fun serialize(encoder: Encoder, value: EstimateFeeResponse) {
-        val jsonObject = buildJsonObject {
-            put("gas_usage", value.gasConsumed.value.toString(10))
-            put("gas_price", value.gasPrice.value.toString(10))
-            put("overall_fee", value.overallFee.value.toString(10))
-        }
-
-        val output = encoder as? JsonEncoder ?: throw SerializationException("")
-
-        output.encodeJsonElement(jsonObject)
-    }
-}
