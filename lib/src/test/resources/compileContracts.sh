3369dd301f436c366f11c64ac9ee7de3c5f0d810
Artur Michalek <artur.michalek@swmansion.com>
2022-09-14 15:02:55 +0200

    Add account contract compilation logic

--- lib/src/test/resources/compileContracts.sh
+++ lib/src/test/resources/compileContracts.sh
@@ -1,11 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
-cd "$(dirname $0)" || exit
+cd "$(dirname "$0")" || exit
 
 for file in src/*.cairo; do
   [ -f "$file" ] || break
 
   name="$(basename -- "$file" .cairo)"
 
-  starknet-compile "$file" --output "compiled/$name.json" --abi "compiled/${name}Abi.json"
+  if [[ $name == *"account"* ]]; then
+    starknet-compile "$file" --account_contract --output "compiled/$name.json" --abi "compiled/${name}Abi.json"
+  else
+    starknet-compile "$file" --output "compiled/$name.json" --abi "compiled/${name}Abi.json"
+  fi
 done
