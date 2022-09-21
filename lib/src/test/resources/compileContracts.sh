9f894190730c2ed2ba89ec9f7ce2b106b189b3df
Artur Michalek <artur.michalek@swmansion.com>
2022-09-21 15:52:45 +0200

    Update compileContracts.sh

--- lib/src/test/resources/compileContracts.sh
+++ lib/src/test/resources/compileContracts.sh
@@ -3,14 +3,11 @@
 cd "$(dirname "$0")" || exit
 mkdir -p "compiled"
 
-# shellcheck disable=SC2044
-for file in $(find src -name "*.cairo" -type f -print); do
-
+while IFS= read -r -d '' file; do
   name="$(basename -- "$file" .cairo)"
-
   if [[ $name == *"account"* ]]; then
     starknet-compile "$file" --account_contract --output "compiled/$name.json" --abi "compiled/${name}Abi.json"
   else
     starknet-compile "$file" --output "compiled/$name.json" --abi "compiled/${name}Abi.json"
   fi
-done
+done < <(find src -name "*.cairo" -type f -print0)
