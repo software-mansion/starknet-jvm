1b6490ad10b791be1a870d872aa49b42cac21039
Artur Michalek <artur.michalek@swmansion.com>
2022-09-27 12:25:29 +0200

    Add random salt generation

--- lib/src/main/kotlin/com/swmansion/starknet/deployercontract/Deployer.kt
+++ lib/src/main/kotlin/com/swmansion/starknet/deployercontract/Deployer.kt
@@ -4,6 +4,7 @@ import com.swmansion.starknet.data.types.Calldata
 import com.swmansion.starknet.data.types.Felt
 import com.swmansion.starknet.provider.Request
 import com.swmansion.starknet.provider.exceptions.RequestFailedException
+import java.security.SecureRandom
 
 data class ContractDeployment(
     val transactionHash: Felt,
@@ -27,7 +28,27 @@ interface Deployer {
      *
      * @throws RequestFailedException
      */
-    fun deployContract(classHash: Felt, unique: Boolean, salt: Felt, constructorCalldata: Calldata): Request<ContractDeployment>
+    fun deployContract(
+        classHash: Felt,
+        unique: Boolean,
+        salt: Felt,
+        constructorCalldata: Calldata,
+    ): Request<ContractDeployment>
+
+    /**
+     * Deploy a contract through Universal Deployer Contract (UDC) with random generated salt and
+     * unique parameter set to true
+     *
+     * @param classHash a class hash of the declared contract
+     * @param constructorCalldata constructor calldata
+     * * @throws RequestFailedException
+     */
+    fun deployContract(classHash: Felt, constructorCalldata: Calldata): Request<ContractDeployment> {
+        val random = SecureRandom()
+        val salt = random.nextLong(1, Long.MAX_VALUE)
+        val feltSalt = Felt(salt)
+        return deployContract(classHash, true, feltSalt, constructorCalldata)
+    }
 
     /**
      * Get a contract address from the deployment
