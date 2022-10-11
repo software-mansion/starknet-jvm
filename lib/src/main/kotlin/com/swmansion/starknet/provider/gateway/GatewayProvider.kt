2a3cc18ae6a605742a54ed8026f523f5b81d8283
Jakub Ptak <jakub.ptak@swmansion.com>
2022-10-11 14:32:26 +0200

    Use toGatewayParam in getNonce

--- lib/src/main/kotlin/com/swmansion/starknet/provider/gateway/GatewayProvider.kt
+++ lib/src/main/kotlin/com/swmansion/starknet/provider/gateway/GatewayProvider.kt
@@ -312,7 +312,10 @@ class GatewayProvider(
     override fun getNonce(contractAddress: Felt): Request<Felt> = getNonce(contractAddress, BlockTag.PENDING)
 
     override fun getNonce(contractAddress: Felt, blockTag: BlockTag): Request<Felt> {
-        val params = listOf("contractAddress" to contractAddress.hexString(), "blockNumber" to blockTag.tag)
+        val params = listOf(
+            "contractAddress" to contractAddress.hexString(),
+            BlockId.Tag(blockTag).toGatewayParam(),
+        )
         val url = feederGatewayRequestUrl("get_nonce")
 
         return HttpRequest(Payload(url, "GET", params), buildDeserializer(Felt.serializer()), httpService)
