7a640129de8af90d0cb289e52d04514f89745174
Jakub Ptak <solpatium003@gmail.com>
2022-10-11 19:26:09 +0200

    Add missing type to estimate fee (#161)

--- lib/src/main/kotlin/com/swmansion/starknet/provider/gateway/GatewayProvider.kt
+++ lib/src/main/kotlin/com/swmansion/starknet/provider/gateway/GatewayProvider.kt
@@ -179,7 +179,7 @@ class GatewayProvider(
         val url = gatewayRequestUrl("add_transaction")
 
         val body = buildJsonObject {
-            put("type", JsonPrimitive("INVOKE_FUNCTION"))
+            put("type", transactionTypeToName(TransactionType.INVOKE))
             put("contract_address", payload.invocation.contractAddress.hexString())
             putJsonArray("calldata") { payload.invocation.calldata.toDecimal().forEach { add(it) } }
             put("max_fee", payload.maxFee.hexString())
@@ -199,7 +199,7 @@ class GatewayProvider(
         val url = gatewayRequestUrl("add_transaction")
 
         val body = buildJsonObject {
-            put("type", "DEPLOY")
+            put("type", transactionTypeToName(TransactionType.DEPLOY))
             put("contract_address_salt", payload.salt)
             putJsonArray("constructor_calldata") {
                 payload.constructorCalldata.toDecimal().forEach { add(it) }
@@ -219,7 +219,7 @@ class GatewayProvider(
         val url = gatewayRequestUrl("add_transaction")
 
         val body = buildJsonObject {
-            put("type", "DECLARE")
+            put("type", transactionTypeToName(TransactionType.DECLARE))
             put("sender_address", DECLARE_SENDER_ADDRESS)
             put("max_fee", payload.maxFee)
             put("nonce", payload.nonce)
@@ -281,6 +281,7 @@ class GatewayProvider(
     ): Request<EstimateFeeResponse> {
         val url = feederGatewayRequestUrl("estimate_fee")
         val body = buildJsonObject {
+            put("type", transactionTypeToName(request.type))
             put("contract_address", request.contractAddress.hexString())
             putJsonArray("calldata") { request.calldata.toDecimal().forEach { add(it) } }
             putJsonArray("signature") { request.signature.toDecimal().forEach { add(it) } }
@@ -373,6 +374,13 @@ class GatewayProvider(
         val payload = GetBlockTransactionCountPayload(BlockId.Number(blockNumber))
 
         return getBlockTransactionCount(payload)
+    }
+
+    // Copied values from TransactionType in cairo-lang
+    private fun transactionTypeToName(type: TransactionType) = when (type) {
+        TransactionType.DECLARE -> "DECLARE"
+        TransactionType.DEPLOY -> "DEPLOY"
+        TransactionType.INVOKE -> "INVOKE_FUNCTION"
     }
 
     companion object Factory {
