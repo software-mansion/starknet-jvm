8f20f9b7d427576f0e1ccdcc2c6327652539f5e7
Artur Michalek <artur.michalek@swmansion.com>
2022-12-08 19:27:13 +0100

    Fix null pointer exception in GatewayTransactionTransformingSerializer

--- lib/src/main/kotlin/com/swmansion/starknet/data/serializers/GatewayTransactionTransformingSerializer.kt
+++ lib/src/main/kotlin/com/swmansion/starknet/data/serializers/GatewayTransactionTransformingSerializer.kt
@@ -1,12 +1,14 @@
 package com.swmansion.starknet.data.serializers
 
 import com.swmansion.starknet.data.types.transactions.*
+import kotlinx.serialization.SerializationException
 import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.JsonTransformingSerializer
 import kotlinx.serialization.json.jsonObject
 
-internal object GatewayTransactionTransformingSerializer : JsonTransformingSerializer<Transaction>(Transaction.serializer()) {
+internal object GatewayTransactionTransformingSerializer :
+    JsonTransformingSerializer<Transaction>(Transaction.serializer()) {
     override fun transformDeserialize(element: JsonElement): JsonElement {
-        return element.jsonObject["transaction"]!!
+        return element.jsonObject["transaction"] ?: throw SerializationException("Transaction missing in response.")
     }
 }
