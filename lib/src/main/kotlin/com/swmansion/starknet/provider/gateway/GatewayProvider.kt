57d385a2a347a173efa25e3344d665510c64950b
Artur Michalek <artur.michalek@swmansion.com>
2022-12-08 16:38:10 +0100

    Rename make_Client to make_Provider

--- lib/src/main/kotlin/com/swmansion/starknet/provider/gateway/GatewayProvider.kt
+++ lib/src/main/kotlin/com/swmansion/starknet/provider/gateway/GatewayProvider.kt
@@ -399,7 +399,7 @@ class GatewayProvider(
 
     companion object Factory {
         @JvmStatic
-        fun makeTestnetClient(): GatewayProvider {
+        fun makeTestnetProvider(): GatewayProvider {
             return GatewayProvider(
                 "$TESTNET_URL/feeder_gateway",
                 "$TESTNET_URL/gateway",
@@ -408,7 +408,7 @@ class GatewayProvider(
         }
 
         @JvmStatic
-        fun makeTestnetClient(testnetId: StarknetChainId): GatewayProvider =
+        fun makeTestnetProvider(testnetId: StarknetChainId): GatewayProvider =
             when (testnetId) {
                 StarknetChainId.TESTNET -> GatewayProvider(
                     "$TESTNET_URL/feeder_gateway",
@@ -424,7 +424,7 @@ class GatewayProvider(
             }
 
         @JvmStatic
-        fun makeTestnetClient(httpService: HttpService): GatewayProvider {
+        fun makeTestnetProvider(httpService: HttpService): GatewayProvider {
             return GatewayProvider(
                 "$TESTNET_URL/feeder_gateway",
                 "$TESTNET_URL/gateway",
@@ -434,7 +434,7 @@ class GatewayProvider(
         }
 
         @JvmStatic
-        fun makeTestnetClient(testnetId: StarknetChainId, httpService: HttpService): GatewayProvider =
+        fun makeTestnetProvider(testnetId: StarknetChainId, httpService: HttpService): GatewayProvider =
             when (testnetId) {
                 StarknetChainId.TESTNET -> GatewayProvider(
                     "$TESTNET_URL/feeder_gateway",
@@ -452,7 +452,7 @@ class GatewayProvider(
             }
 
         @JvmStatic
-        fun makeMainnetClient(): GatewayProvider {
+        fun makeMainnetProvider(): GatewayProvider {
             return GatewayProvider(
                 "$MAINNET_URL/feeder_gateway",
                 "$MAINNET_URL/gateway",
@@ -461,7 +461,7 @@ class GatewayProvider(
         }
 
         @JvmStatic
-        fun makeMainnetClient(httpService: HttpService): GatewayProvider {
+        fun makeMainnetProvider(httpService: HttpService): GatewayProvider {
             return GatewayProvider(
                 "$MAINNET_URL/feeder_gateway",
                 "$MAINNET_URL/gateway",
