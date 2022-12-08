c0c31be321b7abda687f3f6049751099e9ccb300
Artur Michalek <artur.michalek@swmansion.com>
2022-12-08 16:39:11 +0100

    Add `@Throws` to Request interface

--- lib/src/main/kotlin/com/swmansion/starknet/provider/Request.kt
+++ lib/src/main/kotlin/com/swmansion/starknet/provider/Request.kt
@@ -1,6 +1,8 @@
 package com.swmansion.starknet.provider
 
+import com.swmansion.starknet.provider.exceptions.RequestFailedException
 import java.util.concurrent.CompletableFuture
+import kotlin.jvm.Throws
 
 /**
  * An interface implemented by all return values of providers.
@@ -11,6 +13,7 @@ interface Request<T> {
      *
      * @return a result of the request
      */
+    @Throws(RequestFailedException::class)
     fun send(): T
 
     /**
