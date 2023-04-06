2a4367b6880ba6fe9690ada1daa918caa447de5b
Wojciech Szymczyk <wojciech.szymczyk@swmansion.com>
2023-04-06 11:14:43 +0200

    Add kotlin poseidon and tests

---
+++ lib/src/main/kotlin/com/swmansion/starknet/crypto/Poseidon.kt
@@ -0,0 +1,141 @@
+package com.swmansion.starknet.crypto
+
+import com.swmansion.starknet.data.types.Felt
+import com.swmansion.starknet.extensions.toFelt
+import java.math.BigInteger
+
+/**
+ * Split a BigInteger into an array of 4 Long values, each representing a 64-bit chunk of the original BigInteger.
+ *
+ * @param bigInt the BigInteger to split
+ * @return a LongArray containing the 64-bit chunks of the original BigInteger
+ */
+private fun splitBigInteger(bigInt: BigInteger): LongArray {
+    val result = LongArray(4)
+
+    // if the input BigInteger is zero, return an array of zeros
+    if (bigInt == BigInteger.ZERO) {
+        return result
+    }
+    // mask has all bits set to 1 except the least significant one
+    val mask = BigInteger.valueOf(2).pow(64).subtract(BigInteger.ONE)
+
+    // loop through the 64-bit chunks of the BigInteger, shift them and store in the LongArray
+    for (i in 0 until 4) {
+        result[i] = bigInt.shiftRight(i * 64).and(mask).toLong()
+    }
+
+    return result
+}
+
+/**
+ * Convert a LongArray to an Array of BigIntegers.
+ *
+ * @param longArray the LongArray to convert
+ * @return an Array of BigIntegers corresponding to the input LongArray
+ */
+@OptIn(ExperimentalUnsignedTypes::class)
+private fun toBigIntegerArray(longArray: LongArray): Array<BigInteger> {
+    val ulongArray = longArray.map { it.toULong() }.toULongArray()
+    return ulongArray.map { BigInteger(it.toString()) }.toTypedArray()
+}
+
+/**
+ * Combine a LongArray of 64-bit chunks into a single BigInteger.
+ *
+ * @param arr the LongArray containing the 64-bit chunks
+ * @return a BigInteger representing the combined value of the input LongArray
+ */
+private fun unsplitBigInteger(arr: LongArray): BigInteger {
+    val powersOfTwo = listOf(
+            BigInteger.valueOf(2).pow(0),
+            BigInteger.valueOf(2).pow(64),
+            BigInteger.valueOf(2).pow(128),
+            BigInteger.valueOf(2).pow(192)
+    )
+    val barr = toBigIntegerArray(arr)
+    // w * 2**0 + x * 2**64 + y * 2**128 + z * 2**192
+    return barr.zip(powersOfTwo).fold(BigInteger.ZERO) { acc, (b, p) -> acc + b * p }
+}
+
+/**
+ * Starknet poseidon utilities.
+ *
+ * Class with utility methods related to starknet poseidon hash calculation.
+ */
+object Poseidon {
+
+    init {
+        NativeLoader.load("_pos")
+        NativeLoader.load("poseidon_jni")
+    }
+
+    /**
+     * Native hades permutation.
+     */
+    @JvmStatic
+    private external fun hades(
+            values: Array<LongArray>,
+    ): Array<LongArray>
+
+    /**
+     * Compute poseidon hash on single Felt.
+     *
+     * @param x single Felt
+     */
+    @JvmStatic
+    fun poseidonHash(x: Felt): Felt {
+        return unsplitBigInteger(
+                hades(
+                        arrayOf(
+                                splitBigInteger(BigInteger(x.decString())),
+                                longArrayOf(0, 0, 0, 0),
+                                longArrayOf(1, 0, 0, 0)
+                        )
+                )[0]).toFelt
+    }
+
+    /**
+     * Compute poseidon hash on two Felts.
+     *
+     * @param x Felt
+     * @param y Felt
+     */
+    @JvmStatic
+    fun poseidonHash(x: Felt, y: Felt): Felt {
+        return unsplitBigInteger(
+                hades(
+                        arrayOf(
+                                splitBigInteger(BigInteger(x.decString())),
+                                splitBigInteger(BigInteger(y.decString())),
+                                longArrayOf(2, 0, 0, 0)
+                        )
+                )[0]).toFelt
+    }
+
+    /**
+     * Compute poseidon hash on three Felts.
+     *
+     * @param x Felt
+     * @param y Felt
+     * @param z Felt
+     */
+    @JvmStatic
+    fun poseidonHash(x: Felt, y: Felt, z: Felt): Felt {
+        val firstIter = hades(
+                        arrayOf(
+                                splitBigInteger(BigInteger(x.decString())),
+                                splitBigInteger(BigInteger(y.decString())),
+                                splitBigInteger(BigInteger.ZERO)
+                        )
+                )
+
+        val secondIter = hades(arrayOf(
+                splitBigInteger(unsplitBigInteger(firstIter[0]) + BigInteger(z.decString())),
+                splitBigInteger(unsplitBigInteger(firstIter[1]) + BigInteger.ONE),
+                firstIter[2]
+        ))
+
+        return unsplitBigInteger(secondIter[0]).toFelt
+    }
+}
---
+++ lib/src/test/kotlin/starknet/crypto/PoseidonTest.kt
@@ -0,0 +1,73 @@
+package starknet.crypto
+
+import com.swmansion.starknet.crypto.Poseidon
+import com.swmansion.starknet.data.types.Felt
+import com.swmansion.starknet.data.types.Felt.Companion.fromHex
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+import java.math.BigInteger
+
+internal class PoseidonTest {
+
+    @Test
+    fun `poseidon hash single zero`() {
+        val zeroValue = Felt.ZERO
+        val result = Poseidon.poseidonHash(zeroValue)
+        assertEquals(fromHex("0x60009f680a43e6f760790f76214b26243464cdd4f31fdc460baf66d32897c1b"), result)
+    }
+
+    @Test
+    fun `poseidon hash single one`() {
+        val zeroValue = Felt.ONE
+        val result = Poseidon.poseidonHash(zeroValue)
+        assertEquals(fromHex("0x6d226d4c804cd74567f5ac59c6a4af1fe2a6eced19fb7560a9124579877da25"), result)
+    }
+    @Test
+    fun `poseidon hash single big number`() {
+        val value = Felt(BigInteger("737869762948382064636737869762948382064636737869762948382064636"))
+        val result = Poseidon.poseidonHash(value)
+        assertEquals(fromHex("0x1580978ed34d52bfbc78c9f21da6e9df1ed6544bf1dd561616b0aba45a40380"), result)
+    }
+
+    @Test
+    fun `poseidon hash double zero`() {
+        val zeroValue = Felt.ZERO
+        val result = Poseidon.poseidonHash(zeroValue, zeroValue)
+        assertEquals(fromHex("0x293d3e8a80f400daaaffdd5932e2bcc8814bab8f414a75dcacf87318f8b14c5"), result)
+    }
+
+    @Test
+    fun `poseidon hash double big numbers`() {
+        val value = Felt(BigInteger("737869762948382064636737869762948382064636737869762948382064636"))
+        val result = Poseidon.poseidonHash(value, value)
+        assertEquals(fromHex("0x59c0ba54a2613d811726e10be9d6f7e01cf52d6d68ced0d16829027948cdfc3"), result)
+    }
+
+    @Test
+    fun `poseidon hash many all zeros`() {
+        val zeroValue = Felt.ZERO
+        val result = Poseidon.poseidonHash(zeroValue, zeroValue, zeroValue)
+        assertEquals(fromHex("0x29aee7812642221479b7e8af204ceaa5a7b7e113349fc8fb93e6303b477eb4d"), result)
+    }
+
+    @Test
+    fun `poseidon hash many zeros`() {
+        val zeroValue = Felt.ZERO
+        val result = Poseidon.poseidonHash(zeroValue, zeroValue, zeroValue)
+        assertEquals(fromHex("0x29aee7812642221479b7e8af204ceaa5a7b7e113349fc8fb93e6303b477eb4d"), result)
+    }
+
+    @Test
+    fun `poseidon hash many random values`() {
+        val result = Poseidon.poseidonHash(Felt(BigInteger("10")), Felt(BigInteger("8")), Felt(BigInteger("5")))
+        assertEquals(fromHex("0x53aa661c2388b74f48a16163c38893760e26884211599194ffe264f14b5c6e7"), result)
+    }
+
+    @Test
+    fun `poseidon hash many big numbers`() {
+        val value1 = Felt(BigInteger("737869762948382064636737869762948382064636737869762948382064636"))
+        val value2 = Felt(BigInteger("948382064636737869762948382064636737869762948382064636737869762"))
+        val result = Poseidon.poseidonHash(value1, value2, value1)
+        assertEquals(fromHex("0xdaa82261a460722d8deb7d3bb2cb1838084887549df141540b6d88658d34ed"), result)
+    }
+}
