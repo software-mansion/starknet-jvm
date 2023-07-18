e7b7d9cfaca62258c5a82e0e08cb2cb6755ddc9c
Delevo <43750648+DelevoXDG@users.noreply.github.com>
2023-07-18 11:46:41 +0200

    Bump deps (#267)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 
 plugins {
-    id("com.android.library") version "7.2.2" apply false
-    id("com.android.application") version "7.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.7.10" apply false
-    id("org.jetbrains.kotlin.jvm") version "1.7.10" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.7.10" apply false
-    id("org.jetbrains.dokka") version "1.8.10" apply false
-    id("org.jmailen.kotlinter") version "3.11.1"
-    id("org.jetbrains.kotlinx.kover") version "0.5.1"
+    id("com.android.library") version "7.4.0" apply false
+    id("com.android.application") version "7.4.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("org.jetbrains.kotlin.jvm") version "1.9.0" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.0" apply false
+    id("org.jetbrains.dokka") version "1.8.20" apply false
+    id("org.jmailen.kotlinter") version "3.15.0"
+    id("org.jetbrains.kotlinx.kover") version "0.7.2"
 }
