c6f43c51e36460bb5ec873d1cbdc70eea5c72b07
Wojciech Szymczyk <wojciech.szymczyk@swmansion.com>
2023-07-20 15:45:14 +0200

    Revert "Bump deps (#267)"
    This reverts commit e7b7d9cfaca62258c5a82e0e08cb2cb6755ddc9c.

--- androiddemo/build.gradle.kts
+++ androiddemo/build.gradle.kts
@@ -1,11 +1,9 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlinx.kover")
 }
 
 android {
-    namespace = "com.example.androiddemo"
     compileSdk = 32
 
     defaultConfig {
@@ -37,12 +35,12 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.8.0") // highest version available for sdk 32
-    implementation("androidx.appcompat:appcompat:1.5.1") // highest version available for sdk 32
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.4.2")
+    implementation("com.google.android.material:material:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.swmansion.starknet:starknet:0.6.1@aar")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
 }
