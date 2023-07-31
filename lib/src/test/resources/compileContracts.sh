ff8295a90a31055b4a4a8d641737c1979e139755
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2023-07-31 10:54:26 +0200

    Refactor compileContracts.sh

--- lib/src/test/resources/compileContracts.sh
+++ lib/src/test/resources/compileContracts.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-# #!/opt/homebrew/bin/bash
+
+SRC_V0="src_v0"
+SRC_V1="src_v1"
+SRC_V2="src_v2"
+
+COMPILE_V0="compiled_v0"
+COMPILE_V1="compiled_v1"
+COMPILE_V2="compiled_v2"
+
+COMPILER_BIN_V1="cairo_compilers_v1"
+COMPILER_BIN_V2="cairo_compilers_v2"
 
 get_release_url() {
   local VERSION=$1
@@ -19,73 +29,77 @@ get_release_url() {
     OS_DLOAD="apple-darwin"
     ARCH_DLOAD="aarch64"
     EXT="tar"
+  else
+    echo "Unsupported OS: $OS"
+    exit 1
   fi
 
-#  if [ "$ARCH" == "x86_64" ]; then
-#    ARCH_DLOAD="x86_64"
-#  elif [ "$ARCH" == "arm64" ]; then
-#    ARCH_DLOAD="aarch64"
-#  fi
+  #  if [ "$ARCH" == "x86_64" ]; then
+  #    ARCH_DLOAD="x86_64"
+  #  elif [ "$ARCH" == "arm64" ]; then
+  #    ARCH_DLOAD="aarch64"
+  #  fi
 
   echo "https://github.com/starkware-libs/cairo/releases/download/v${VERSION}/release-${ARCH_DLOAD}-${OS_DLOAD}.${EXT}"
 }
 
 fetch_compilers() {
   local VERSION=$1
-  local FOLDER=$2
+  local OUT_DIR=$2
   local COMPILER_URL=$(get_release_url "$VERSION")
 
-  echo "Fetching compiler binaries for Cairo $VERSION..."
+  echo "Fetching compiler v$VERSION binaries..."
   echo "URL: $COMPILER_URL"
-  mkdir -p "$FOLDER"
-  curl -LsSf "$COMPILER_URL" | tar -xz -C "$FOLDER"
 
-  COMPILER_PATH=$(pwd)/"$FOLDER"/cairo/bin/starknet-compile
-  SIERRA_PATH=$(pwd)/"$FOLDER"/cairo/bin/starknet-sierra-compile
+  mkdir -p "$OUT_DIR"
+  curl -LsSf "$COMPILER_URL" | tar -x -C "$OUT_DIR" || exit 1
+
+  COMPILER_PATH=$(pwd)/"$OUT_DIR"/cairo/bin/starknet-compile
+  SIERRA_PATH=$(pwd)/"$OUT_DIR"/cairo/bin/starknet-sierra-compile
 
   echo "Done!"
 }
 
 pushd "$(dirname "$0")" || exit
-mkdir -p "compiled_v0"
+mkdir -p "$COMPILE_V0"
 
 echo "Compiling v0 contracts.."
 
 while IFS= read -r -d '' file; do
   name="$(basename -- "$file" .cairo)"
   if [[ $name == *"account"* ]]; then
-    starknet-compile-deprecated "$file" --account_contract --output "compiled_v0/$name.json" --abi "compiled_v0/${name}Abi.json"
+    starknet-compile-deprecated "$file" --account_contract --output "$COMPILE_V0/$name.json" --abi "$COMPILE_V0/${name}Abi.json" || exit 1
   else
-    starknet-compile-deprecated "$file" --output "compiled_v0/$name.json" --abi "compiled_v0/${name}Abi.json"
+    starknet-compile-deprecated "$file" --output "$COMPILE_V0/$name.json" --abi "$COMPILE_V0/${name}Abi.json" || exit 1
   fi
-done < <(find src_v0 -name "*.cairo" -type f -print0)
+done < <(find "$SRC_V0" -name "*.cairo" -type f -print0)
 popd
 echo "Done!"
 
 pushd "$(dirname "$0")" || exit 1
-fetch_compilers "1.1.1" "cairo_compilers_v1"
-mkdir -p "compiled_v1"
+fetch_compilers "1.1.1" "$COMPILER_BIN_V1"
+mkdir -p "$COMPILE_V1"
 
 echo "Compiling v1 contracts.."
 
 while IFS= read -r -d '' file; do
   name="$(basename -- "$file" .cairo)"
-  $COMPILER_PATH --allowed-libfuncs-list-name experimental_v0.1.0 "$file" "compiled_v1/$name.json"
-  $SIERRA_PATH --allowed-libfuncs-list-name experimental_v0.1.0 --add-pythonic-hints "compiled_v1/$name.json" "compiled_v1/$name.casm"
-done < <(find src_v1 -name "*.cairo" -type f -print0)
+  $COMPILER_PATH --allowed-libfuncs-list-name experimental_v0.1.0 "$file" "$COMPILE_V1/$name.json" || exit 1
+  $SIERRA_PATH --allowed-libfuncs-list-name experimental_v0.1.0 --add-pythonic-hints "$COMPILE_V1/$name.json" "$COMPILE_V1/$name.casm" || exit 1
+done < <(find "$SRC_V1" -name "*.cairo" -type f -print0)
 popd
 echo "Done!"
 
 pushd "$(dirname "$0")" || exit 1
-fetch_compilers "2.0.0" "cairo_compilers_v2"
-mkdir -p "compiled_v2"
+fetch_compilers "2.0.0" "$COMPILER_BIN_V2"
+mkdir -p "$COMPILE_V2"
 
 echo "Compiling v2 contracts.."
 
 while IFS= read -r -d '' file; do
   name="$(basename -- "$file" .cairo)"
-  $COMPILER_PATH "$file" "compiled_v2/$name.json"
-  $SIERRA_PATH --add-pythonic-hints "compiled_v2/$name.json" "compiled_v2/$name.casm"
-done < <(find src_v2 -name "*.cairo" -type f -print0)
+  $COMPILER_PATH "$file" "$COMPILE_V2/$name.json" || exit 1
+  $SIERRA_PATH --add-pythonic-hints "$COMPILE_V2/$name.json" "$COMPILE_V2/$name.casm" || exit 1
+done < <(find "$SRC_V2" -name "*.cairo" -type f -print0)
 popd
 echo "Done!"
