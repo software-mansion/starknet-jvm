05498f026e7623be7c7f87b692b84a6cc8b4ede3
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2023-08-04 17:00:05 +0200

    Add @SerialName's to Message dataclasses

--- lib/src/main/kotlin/com/swmansion/starknet/data/types/Messages.kt
+++ lib/src/main/kotlin/com/swmansion/starknet/data/types/Messages.kt
@@ -2,14 +2,17 @@ package com.swmansion.starknet.data.types
 
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.json.JsonNames
 
 @OptIn(ExperimentalSerializationApi::class)
 @Serializable
 data class GatewayMessageL2ToL1(
     @JsonNames("to_address")
+    @SerialName("to_address")
     val toAddress: Felt,
 
+    @SerialName("payload")
     @JsonNames("payload")
     val payload: List<Felt>,
 )
@@ -18,9 +21,11 @@ data class GatewayMessageL2ToL1(
 @Serializable
 data class GatewayMessageL1ToL2(
     @JsonNames("from_address")
+    @SerialName("from_address")
     val fromAddress: Felt,
 
     @JsonNames("payload")
+    @SerialName("payload")
     val payload: List<Felt>,
 )
 
@@ -28,12 +33,15 @@ data class GatewayMessageL1ToL2(
 @Serializable
 data class RpcMessageL2ToL1(
     @JsonNames("from_address")
+    @SerialName("from_address")
     val fromAddress: Felt,
 
     @JsonNames("to_address")
+    @SerialName("to_address")
     val toAddress: Felt,
 
     @JsonNames("payload")
+    @SerialName("payload")
     val payload: List<Felt>,
 )
 
@@ -41,14 +49,18 @@ data class RpcMessageL2ToL1(
 @Serializable
 data class RpcMessageL1ToL2(
     @JsonNames("from_address")
+    @SerialName("from_address")
     val fromAddress: Felt,
 
     @JsonNames("to_address")
+    @SerialName("to_address")
     val toAddress: Felt,
 
     @JsonNames("entry_point_selector")
+    @SerialName("entry_point_selector")
     val selector: Felt,
 
     @JsonNames("payload")
+    @SerialName("payload")
     val payload: List<Felt>,
 )
