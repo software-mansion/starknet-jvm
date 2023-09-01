2a14c1851a12bec4d479e32994722a56c366608a
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2023-09-01 18:37:37 +0200

    Re-enable disabled simulateTransactions test

--- lib/src/test/kotlin/starknet/account/StandardAccountTest.kt
+++ lib/src/test/kotlin/starknet/account/StandardAccountTest.kt
@@ -733,8 +733,6 @@ class StandardAccountTest {
         }
     }
 
-    // TODO: Re-enable if devnet.py is updated to support this or devnet-rs is released and supports trace api
-    @Disabled
     @Test
     fun `simulate transactions`() {
         val account = StandardAccount(accountAddress, signer, rpcProvider)
