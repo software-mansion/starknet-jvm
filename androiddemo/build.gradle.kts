3b6818ffec1e6ec735c42dd042c6c6ee3d2f5626
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2023-10-03 15:01:45 +0200

    Fix android demo deps: set isTransitive=true

--- androiddemo/build.gradle.kts
+++ androiddemo/build.gradle.kts
@@ -39,7 +39,9 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.4.2")
     implementation("com.google.android.material:material:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.swmansion.starknet:starknet:0.7.1@aar")
+    implementation("com.swmansion.starknet:starknet:0.7.1@aar"){
+        isTransitive = true
+    }
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
