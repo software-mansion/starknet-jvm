65109e5d814a48f85f1259c78a23af198622feb9
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2023-10-04 16:17:01 +0200

    Add coroutines

--- androiddemo/build.gradle.kts
+++ androiddemo/build.gradle.kts
@@ -44,9 +44,12 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.4.2")
     implementation("com.google.android.material:material:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    // Make sure you are using the AAR and not a JAR and include transitive dependencies
     implementation("com.swmansion.starknet:starknet:0.7.1@aar"){
         isTransitive = true
     }
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.6.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
