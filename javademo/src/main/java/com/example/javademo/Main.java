5113c774d48ac798b7ab12544060bbc483a785e0
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2023-10-13 15:10:25 +0200

    Update javademo (#335)

--- javademo/src/main/java/com/example/javademo/Main.java
+++ javademo/src/main/java/com/example/javademo/Main.java
@@ -15,8 +15,6 @@ import com.swmansion.starknet.provider.Provider;
 import com.swmansion.starknet.provider.Request;
 import com.swmansion.starknet.provider.rpc.JsonRpcProvider;
 
-import static com.swmansion.starknet.data.Selector.selectorFromName;
-
 import java.io.IOException;
 import java.math.BigInteger;
 import java.nio.file.Files;
@@ -27,15 +25,22 @@ import java.util.List;
 
 
 public class Main {
+    private static class DemoConfig {
+        // Please set the environment variables or replace with actual values manually
+        public static String rpcNodeUrl = System.getenv().getOrDefault("STARKNET_RPC_URL", "https://example-node-url.com/rpc");
+        public static String accountAddress = System.getenv().getOrDefault("STARKNET_ACCOUNT_ADDRESS", "0x123456");
+        public static String accountPrivateKey = System.getenv().getOrDefault("STARKNET_PRIVATE_KEY", "0x789");
+    }
+
     public static void main(String[] args) throws Exception {
         // Create a provider for interacting with Starknet
-        JsonRpcProvider provider = new JsonRpcProvider("https://example-node-url.com/rpc", StarknetChainId.TESTNET);
+        JsonRpcProvider provider = new JsonRpcProvider(DemoConfig.rpcNodeUrl, StarknetChainId.TESTNET);
 
         // Set up an account
         // Please note the account must be deployed and have enough funds to paying the fees
         // ⚠️ WARNING ⚠️ Both the account address and private key are for demonstration purposes only.
-        Felt address = Felt.fromHex("0x123456");    //Please replace with the actual value
-        Felt privateKey = Felt.fromHex("0x789");    //Please replace with the actual value
+        Felt address = Felt.fromHex(DemoConfig.accountAddress);
+        Felt privateKey = Felt.fromHex(DemoConfig.accountPrivateKey);
         // Make sure to check Cairo version of account contract
         Account account = new StandardAccount(address, privateKey, provider, Felt.ZERO);
 
@@ -45,7 +50,7 @@ public class Main {
         // Specify the contract address, in this example ERC-20 ETH contract is used
         Felt erc20ContractAddress = Felt.fromHex("0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7");
         // Create a call
-        List<Felt> calldata = List.of(erc20ContractAddress, amount.getLow(), amount.getHigh());
+        List<Felt> calldata = List.of(recipientAccountAddress, amount.getLow(), amount.getHigh());
         Call invokeCall = new Call(erc20ContractAddress, "transfer", calldata);
         // Or using any objects implementing ConvertibleToCalldata interface
         Call callFromCallArguments = Call.fromCallArguments(
@@ -56,11 +61,13 @@ public class Main {
 
         Request<InvokeFunctionResponse> executeRequest = account.execute(invokeCall);
         InvokeFunctionResponse executeResponse = executeRequest.send();
+        Thread.sleep(10000); // Wait for invoke tx to complete
 
         // Make sure that the transaction succeeded
         Request<? extends TransactionReceipt> receiptRequest = provider.getTransactionReceipt(executeResponse.getTransactionHash());
         TransactionReceipt receipt = receiptRequest.send();
         Boolean isAccepted = receipt.isAccepted();
+        System.out.println(isAccepted);
 
         // Call contract (Get ETH balance)
         Call call = new Call(erc20ContractAddress, "balanceOf", List.of(account.getAddress()));
@@ -69,18 +76,29 @@ public class Main {
         List<Felt> callResponse = callRequest.send();
         // Output value's type is UInt256 and is represented by two Felt values
         Uint256 balance = new Uint256(callResponse.get(0), callResponse.get(1));
+        System.out.println(balance);
 
-        // Declare cairo 1 contract
+        // Declare Cairo 0 contract
+        // Aside from contract code, you will additionaly need to provide classHash of said contract
+        // Class hash is calculated using the tools you used for compilation
+
+        Felt cairo0ContractClassHash = Felt.fromHex("0x3b32bb615844ea7a9a56a8966af1a5ba1457b1f5c9162927ca1968975b0d2a9");
+        Path cairo0ContractPath = Paths.get("javademo/src/main/resources/contracts_v0/target/release/balance.json");
+        DeclareResponse cairo0DeclareResponse = declareCairo0Contract(account, provider, cairo0ContractPath, cairo0ContractClassHash);
+
+        // TODO: (#336) re-enable once loading posedion from jar is fixed
+        // Declare Cairo 1 contract
         // You need to provide both sierra and casm codes of compiled contracts
-        DeclareResponse declareResponse1 = declareCairo1Contract(account, provider, Paths.get("balance_v1.sierra.json"), Paths.get("balance_v1.casm.json"));
-        Felt classHashCairo1Contract = declareResponse1.getClassHash();
+        // Path sierraPath = Paths.get("javademo/src/main/resources/contracts/target/release/demo_Balance.sierra.json");
+        // Path casmPath = Paths.get("javademo/src/main/resources/contracts/target/release/demo_Balance.casm.json");
+        // DeclareResponse cairo1DeclareResponse = declareCairo1Contract(account, provider, sierraPath, casmPath);
+        // Felt cairo1ContractClassHash = cairo1DeclareResponse.getClassHash();
+        // Thread.sleep(15000); // Wait for declare tx to complete
 
         // Deploy a contract with Universal Deployer Contract (for both cairo 1 and cairo 0 contracts)
-        DeployContractResult deployContractResult = deployContract(account, provider, classHashCairo1Contract, Collections.emptyList());
+        DeployContractResult deployContractResult = deployContract(account, provider, cairo0ContractClassHash, Collections.emptyList());
         Felt deployedContractAddress = deployContractResult.contractAddress;
-
-        // Check the initial value of `balance` in deployed contract
-        provider.getStorageAt(deployedContractAddress, selectorFromName("balance"));
+        Thread.sleep(15000); // Wait for deploy tx to complete
 
         // Manually sign a hash
         Felt hash = Felt.fromHex("0x121212121212");
@@ -93,6 +111,28 @@ public class Main {
 
         // Verify a signature
         boolean isCorrect = StarknetCurve.verify(publicKey, hash, r, s);
+        System.out.println(isCorrect);
+    }
+
+    private static DeclareResponse declareCairo0Contract(Account account, Provider provider, Path contractPath, Felt classHash) throws IOException {
+        // Read contract source code
+        String contractCode = String.join("", Files.readAllLines(contractPath));
+        Cairo0ContractDefinition contractDefinition = new Cairo0ContractDefinition(contractCode);
+
+        Felt nonce = account.getNonce().send();
+
+        // Estimate fee for declaring a contract
+        DeclareTransactionPayload declareTransactionPayloadForFeeEstimate = account.signDeclare(contractDefinition, classHash, new ExecutionParams(nonce, new Felt(1000000000000000L)), false);
+        Request<List<EstimateFeeResponse>> feeEstimateRequest = provider.getEstimateFee(List.of(declareTransactionPayloadForFeeEstimate));
+        Felt feeEstimate = feeEstimateRequest.send().get(0).getOverallFee();
+        Felt maxFee = new Felt(feeEstimate.getValue().multiply(BigInteger.TWO));
+        // Make sure to prefund the account with enough funds to cover the fee for declare transaction
+
+        ExecutionParams executionParams = new ExecutionParams(nonce, maxFee);
+        DeclareTransactionV1Payload payload = account.signDeclare(contractDefinition, classHash, executionParams, false);
+        Request<DeclareResponse> request = provider.declareContract(payload);
+
+        return request.send();
     }
 
     private static DeclareResponse declareCairo1Contract(Account account, Provider provider, Path contractPath, Path casmPath) throws IOException {
---
+++ javademo/src/main/resources/contracts_v0/src/balance.cairo
@@ -0,0 +1,32 @@
+// Declare this file as a Starknet contract.
+%lang starknet
+
+from starkware.cairo.common.cairo_builtins import HashBuiltin
+
+// Define a storage variable.
+@storage_var
+func balance() -> (res: felt) {
+}
+
+// Define an event.
+@event
+func balance_increased(by_amount: felt) {
+}
+
+// Increases the balance by the given amount.
+@external
+func increase_balance{syscall_ptr: felt*, pedersen_ptr: HashBuiltin*, range_check_ptr}(
+    amount: felt
+) {
+    let (res) = balance.read();
+    balance.write(res + amount);
+    balance_increased.emit(by_amount=amount);
+    return ();
+}
+
+// Returns the current balance.
+@view
+func get_balance{syscall_ptr: felt*, pedersen_ptr: HashBuiltin*, range_check_ptr}() -> (res: felt) {
+    let (res) = balance.read();
+    return (res=res);
+}
