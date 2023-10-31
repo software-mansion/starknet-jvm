5c974081871d406470229055030cc8c180abeded
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2023-10-31 22:03:55 +0100

    Add FeltArray data class

---
+++ lib/src/main/kotlin/com/swmansion/starknet/data/types/FeltArray.kt
@@ -0,0 +1,11 @@
+package com.swmansion.starknet.data.types
+
+import com.swmansion.starknet.data.types.conversions.ConvertibleToCalldata
+
+data class FeltArray(private val list: MutableList<Felt>) : ConvertibleToCalldata, MutableList<Felt> by list {
+    constructor(vararg elements: Felt) : this(elements.toMutableList())
+    constructor(collection: Collection<Felt>) : this(collection.toMutableList())
+    constructor() : this(emptyList())
+
+    override fun toCalldata(): List<Felt> = list.toList()
+}
