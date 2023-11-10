02ab6f74370a64c97b734d9431cd79b17ae008d3
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2023-11-10 14:18:00 +0100

    Format

--- lib/src/main/kotlin/com/swmansion/starknet/data/serializers/TypedDataTypeBaseSerializer.kt
+++ lib/src/main/kotlin/com/swmansion/starknet/data/serializers/TypedDataTypeBaseSerializer.kt
@@ -8,7 +8,7 @@ import kotlinx.serialization.json.*
 internal object TypedDataTypeBaseSerializer : JsonContentPolymorphicSerializer<TypeBase>(TypeBase::class) {
     override fun selectDeserializer(element: JsonElement): DeserializationStrategy<out TypeBase> {
         val type = element.jsonObject["type"]?.jsonPrimitive?.content
-        
+
         return when (type) {
             "merkletree" -> TypedData.MerkleTreeType.serializer()
             is String -> TypedData.Type.serializer()
