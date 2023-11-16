f6a123a8f72a33a123bbf27d025194bef39476d6
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2023-11-16 12:34:51 +0100

    Bump version to 0.8.0

--- javademo/build.gradle.kts
+++ javademo/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 dependencies {
-    implementation("com.swmansion.starknet:starknet:0.7.3")
+    implementation("com.swmansion.starknet:starknet:0.8.0")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.0")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.10.0")
 }
--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -8,7 +8,7 @@
 
 import org.jetbrains.dokka.gradle.DokkaTask
 
-version = "0.7.3"
+version = "0.8.0"
 group = "com.swmansion.starknet"
 
 plugins {
