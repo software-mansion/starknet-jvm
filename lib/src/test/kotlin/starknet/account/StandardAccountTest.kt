21018a2309e74fa6ee368f89804c5026340b2dc6
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2023-11-30 20:53:01 +0100

    Format

--- lib/src/test/kotlin/starknet/account/StandardAccountTest.kt
+++ lib/src/test/kotlin/starknet/account/StandardAccountTest.kt
@@ -16,7 +16,6 @@ import com.swmansion.starknet.signer.StarkCurveSigner
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.parallel.Execution
 import org.junit.jupiter.api.parallel.ExecutionMode
