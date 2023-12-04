489836b07169d804f792bc565140fe57f134fb82
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2023-12-04 20:31:13 +0100

    Separate JsonRpcError's; revertError -> data in Exceptions

--- lib/src/main/kotlin/com/swmansion/starknet/provider/exceptions/RpcRequestFailedException.kt
+++ lib/src/main/kotlin/com/swmansion/starknet/provider/exceptions/RpcRequestFailedException.kt
@@ -6,7 +6,7 @@ package com.swmansion.starknet.provider.exceptions
  * @param code error code returned by the rpc provider
  * @param message error message returned by the rpc provider
  * @param payload payload returned by the service used to communicate with Starknet
- * @param revertError revert error returned by the rpc provider
+ * @param data data returned by the rpc provider
  */
-class RpcRequestFailedException(val code: Int, message: String, revertError: String? = null, payload: String) :
-    RequestFailedException(message = message, revertError = revertError, payload = payload)
+class RpcRequestFailedException(val code: Int, message: String, data: String? = null, payload: String) :
+    RequestFailedException(message = message, data = data, payload = payload)
