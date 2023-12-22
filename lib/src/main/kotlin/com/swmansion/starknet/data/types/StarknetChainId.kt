cfc065ac0e716bd1a726ef13c72b5a8859334cb8
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2023-12-22 14:17:17 +0100

    Add SEPOLIA_TESTNET and SEPOLIA_INTEGRATION to StarknetChainId

--- lib/src/main/kotlin/com/swmansion/starknet/data/types/StarknetChainId.kt
+++ lib/src/main/kotlin/com/swmansion/starknet/data/types/StarknetChainId.kt
@@ -3,4 +3,6 @@ package com.swmansion.starknet.data.types
 enum class StarknetChainId(val value: Felt) {
     MAINNET(Felt.fromHex("0x534e5f4d41494e")), // encodeShortString('SN_MAIN'),
     TESTNET(Felt.fromHex("0x534e5f474f45524c49")), // encodeShortString('SN_GOERLI'),
+    SEPOLIA_TESTNET(Felt.fromHex("0x534e5f5345504f4c4941")), // encodeShortString('SN_SEPOLIA'),
+    SEPOLIA_INTEGRATION(Felt.fromHex("0x534e5f494e544547524154494f4e5f5345504f4c4941")), // encodeShortString('SN_INTEGRATION_SEPOLIA'),
 }
