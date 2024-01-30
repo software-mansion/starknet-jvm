5d9f0f55bfa0db077ec79106b67f8922e40b3340
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2024-01-30 13:57:39 +0100

    Add `calculate data availability modes` test

--- lib/src/test/kotlin/com/swmansion/starknet/data/TransactionHashCalculatorTest.kt
+++ lib/src/test/kotlin/com/swmansion/starknet/data/TransactionHashCalculatorTest.kt
@@ -2,6 +2,7 @@ package com.swmansion.starknet.data
 
 import com.swmansion.starknet.data.types.Felt
 import com.swmansion.starknet.data.types.StarknetChainId
+import com.swmansion.starknet.data.types.transactions.DAMode
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
@@ -12,7 +13,7 @@ internal class TransactionHashCalculatorTest {
     private val version = Felt.ONE
 
     @Test
-    fun calculateInvokeTxHash() {
+    fun `calculate invoke v1 transaction hash`() {
         val hash = TransactionHashCalculator.calculateInvokeTxV1Hash(
             contractAddress = Felt.fromHex("0x6352037a8acbb31095a8ed0f4aa8d8639e13b705b043a1b08f9640d2f9f0d56"),
             calldata = calldata,
@@ -27,7 +28,7 @@ internal class TransactionHashCalculatorTest {
     }
 
     @Test
-    fun calculateDeployAccountTxHash() {
+    fun `calculate deploy account v1 transaction hash`() {
         val hash = TransactionHashCalculator.calculateDeployAccountV1TxHash(
             classHash = Felt.fromHex("0x21a7f43387573b68666669a0ed764252ce5367708e696e31967764a90b429c2"),
             calldata = calldata,
@@ -40,5 +41,32 @@ internal class TransactionHashCalculatorTest {
         val expected = Felt.fromHex("0x68beaf15e356928a1850cf343be85032efad964324b0abca4a9a57ff2057ef7")
 
         assertEquals(expected, hash)
+    }
+
+    @Test
+    fun `calculate data availability modes`(){
+        val result = TransactionHashCalculator.dataAvailabilityModes(
+            feeDataAvailabilityMode = DAMode.L1,
+            nonceDataAvailabilityMode = DAMode.L1,
+        )
+        assertEquals(Felt.ZERO, result)
+
+        val result2 = TransactionHashCalculator.dataAvailabilityModes(
+            feeDataAvailabilityMode = DAMode.L2,
+            nonceDataAvailabilityMode = DAMode.L1,
+        )
+        assertEquals(Felt.ONE, result2)
+
+        val result3 = TransactionHashCalculator.dataAvailabilityModes(
+            feeDataAvailabilityMode = DAMode.L1,
+            nonceDataAvailabilityMode = DAMode.L2,
+        )
+        assertEquals(Felt.fromHex("0x100000000"), result3)
+
+        val result4 = TransactionHashCalculator.dataAvailabilityModes(
+            feeDataAvailabilityMode = DAMode.L2,
+            nonceDataAvailabilityMode = DAMode.L2,
+        )
+        assertEquals(Felt.fromHex("0x100000001"), result4)
     }
 }
