0141f7636a76ca28bb12d08a24748a5ce513d746
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2024-02-13 16:50:34 +0100

    Allow custom values in StarknetChainId   - Change StarknetChainId to final class   - Use companion object for default values

--- lib/src/main/kotlin/com/swmansion/starknet/data/types/StarknetChainId.kt
+++ lib/src/main/kotlin/com/swmansion/starknet/data/types/StarknetChainId.kt
@@ -1,20 +1,42 @@
 package com.swmansion.starknet.data.types
 
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-enum class StarknetChainId(val value: Felt) {
-    @SerialName("0x534e5f4d41494e")
-    MAINNET(Felt.fromHex("0x534e5f4d41494e")), // encodeShortString('SN_MAIN'),
+class StarknetChainId private constructor(val value: Felt) {
+    companion object {
+        @field:JvmField
+        val MAIN = StarknetChainId(Felt.fromHex("0x534e5f4d41494e")) // encodeShortString('SN_MAIN'),
 
-    @Deprecated("Consider using SEPOLIA instead")
-    @SerialName("0x534e5f474f45524c49")
-    GOERLI(Felt.fromHex("0x534e5f474f45524c49")), // encodeShortString('SN_GOERLI'),
+        @field:JvmField
+        val GOERLI = StarknetChainId(Felt.fromHex("0x534e5f474f45524c49")) // encodeShortString('SN_GOERLI'),
 
-    @SerialName("0x534e5f5345504f4c4941")
-    SEPOLIA(Felt.fromHex("0x534e5f5345504f4c4941")), // encodeShortString('SN_SEPOLIA'),]
+        @field:JvmField
+        val SEPOLIA = StarknetChainId(Felt.fromHex("0x534e5f5345504f4c4941")) // encodeShortString('SN_SEPOLIA'),]
 
-    @SerialName("0x534e5f494e544547524154494f4e5f5345504f4c4941")
-    INTEGRATION_SEPOLIA(Felt.fromHex("0x534e5f494e544547524154494f4e5f5345504f4c4941")), // encodeShortString('SN_INTEGRATION_SEPOLIA'),
+        @field:JvmField
+        val INTEGRATION_SEPOLIA =
+            StarknetChainId(Felt.fromHex("0x534e5f494e544547524154494f4e5f5345504f4c4941")) // encodeShortString('SN_INTEGRATION_SEPOLIA'),
+
+        @JvmStatic
+        fun fromShortString(shortString: String): StarknetChainId {
+            return fromFelt(Felt.fromShortString(shortString))
+        }
+
+        @JvmStatic
+        fun fromHex(hex: String): StarknetChainId {
+            return fromFelt(Felt.fromHex(hex))
+        }
+
+        @JvmStatic
+        fun fromFelt(felt: Felt): StarknetChainId {
+            return when (felt) {
+                MAIN.value -> MAIN
+                GOERLI.value -> GOERLI
+                SEPOLIA.value -> SEPOLIA
+                INTEGRATION_SEPOLIA.value -> INTEGRATION_SEPOLIA
+                else -> StarknetChainId(felt)
+            }
+        }
+    }
 }
