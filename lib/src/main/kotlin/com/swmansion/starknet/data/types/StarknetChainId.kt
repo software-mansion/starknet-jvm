efabb38ca433c591f57c08033f62ed02d7658b97
Maksim Zdobnikau <43750648+DelevoXDG@users.noreply.github.com>
2024-02-15 11:15:43 +0100

    Update `StarknetChainId` docs (#420)

--- lib/src/main/kotlin/com/swmansion/starknet/data/types/StarknetChainId.kt
+++ lib/src/main/kotlin/com/swmansion/starknet/data/types/StarknetChainId.kt
@@ -18,16 +18,33 @@ data class StarknetChainId(val value: Felt) {
         @field:JvmField
         val INTEGRATION_SEPOLIA = StarknetChainId(Felt.fromHex("0x534e5f494e544547524154494f4e5f5345504f4c4941")) // encodeShortString('SN_INTEGRATION_SEPOLIA'),
 
+        /**
+         * Create [StarknetChainId] from a network name represented as ASCII string.
+         *
+         * @param networkName string transformed to [StarknetChainId]
+         */
         @JvmStatic
         fun fromNetworkName(networkName: String): StarknetChainId {
             return StarknetChainId(Felt.fromShortString(networkName))
         }
 
+        /**
+         * Create [StarknetChainId] from a hex string.
+         *
+         * @param hex string transformed to [StarknetChainId]
+         */
         @JvmStatic
         fun fromHex(hex: String): StarknetChainId {
             return StarknetChainId(Felt.fromHex(hex))
         }
     }
+
+    /**
+     * Transform [StarknetChainId] to a network name represented as ASCII string.
+     * Example: [StarknetChainId.SEPOLIA] -> "SN_SEPOLIA"
+     *
+     * @return network name represented as ASCII string
+     */
     fun toNetworkName(): String {
         return value.toShortString()
     }
