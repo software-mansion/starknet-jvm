9111651151e1d0c64a00d49c8cbb021994521d6d
Maksim Zdobnikau <maksim.zdobnikau@swmansion.com>
2024-03-06 18:20:22 +0100

    Fix serialization process for `enum` type
    - Support revision 0 type named enum

--- lib/src/main/kotlin/com/swmansion/starknet/data/serializers/TypedDataTypeBaseSerializer.kt
+++ lib/src/main/kotlin/com/swmansion/starknet/data/serializers/TypedDataTypeBaseSerializer.kt
@@ -11,7 +11,10 @@ internal object TypedDataTypeBaseSerializer : JsonContentPolymorphicSerializer<T
 
         return when (type) {
             "merkletree" -> TypedData.MerkleTreeType.serializer()
-            "enum" -> TypedData.EnumType.serializer()
+            "enum" -> when (element.jsonObject["contains"]?.jsonPrimitive?.content) {
+                null -> TypedData.Type.serializer()
+                else -> TypedData.EnumType.serializer()
+            }
             is String -> TypedData.Type.serializer()
             null -> throw IllegalArgumentException("Input element does not contain mandatory field 'type'")
             else -> throw IllegalArgumentException("Unknown type '$type'")
