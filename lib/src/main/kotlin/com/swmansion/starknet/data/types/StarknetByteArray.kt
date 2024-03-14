7a1e543c8eb50ad55fe24f156ed4cd0b73193bb6
Maksim Zdobnikau <43750648+DelevoXDG@users.noreply.github.com>
2024-03-14 14:49:17 +0300

    Add StarknetByteArray docs (#438)

--- lib/src/main/kotlin/com/swmansion/starknet/data/types/StarknetByteArray.kt
+++ lib/src/main/kotlin/com/swmansion/starknet/data/types/StarknetByteArray.kt
@@ -4,16 +4,33 @@ import com.swmansion.starknet.data.types.conversions.ConvertibleToCalldata
 import com.swmansion.starknet.extensions.splitToShortStrings
 import com.swmansion.starknet.extensions.toFelt
 
+/**
+ * Represents a ByteArray struct from Cairo.
+ *
+ * The ByteArray struct is used to represent a string in Cairo.
+ *
+ * @param data The list of 31-byte chunks of the byte array
+ * @param pendingWord The last chunk of the byte array, which consists of at most 30 bytes
+ * @param pendingWordLen The number of bytes in [pendingWord]
+ */
 data class StarknetByteArray(
     val data: List<Felt>,
     val pendingWord: Felt,
     val pendingWordLen: Int,
 ) : ConvertibleToCalldata {
+    /**
+     * Encode as a Felt list
+     */
     override fun toCalldata(): List<Felt> {
         return listOf(data.size.toFelt) + data + listOf(pendingWord, pendingWordLen.toFelt)
     }
 
     companion object {
+        /**
+         * Create byte array from a string.
+         *
+         * @param string The string to be transformed to byte array
+         */
         @JvmStatic
         fun fromString(string: String): StarknetByteArray {
             val shortStrings = string.splitToShortStrings()
