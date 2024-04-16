eb043435bb57fd8b7df1037a49b40e03803af1d8
Fiiranek <jellybean@onet.eu>
2024-04-16 10:56:30 +0200

    Remove v0 contracts and compile scripts

--- lib/src/test/resources/compileContracts.sh
+++ lib/src/test/resources/compileContracts.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-readonly V0_CONTRACT_PATH="contracts_v0"
 readonly V1_CONTRACT_PATH="contracts_v1"
 readonly V2_CONTRACT_PATH="contracts_v2"
 readonly V2_6_CONTRACT_PATH="contracts_v2_6"
-
-readonly V0_ARTIFACT_PATH="contracts_v0/target/release"
 
 readonly V1_SCARB_VERSION="0.4.0"
 readonly V2_SCARB_VERSION="0.7.0"
@@ -14,21 +11,6 @@ readonly V2_6_SCARB_VERSION="2.6.0"
 # TODO: (#365) Use asdf action to install scarb instead
 echo "Installing scarb..."
 asdf plugin add scarb || true
-echo "Done!"
-
-pushd "$(dirname "$0")" || exit 1
-mkdir -p "$V0_ARTIFACT_PATH"
-echo "Compiling v0 contracts.."
-
-while IFS= read -r -d '' file; do
-  name="$(basename -- "$file" .cairo)"
-  if [[ $name == *"account"* ]]; then
-    starknet-compile-deprecated "$file" --account_contract --output "$V0_ARTIFACT_PATH/$name.json" --abi "$V0_ARTIFACT_PATH/${name}Abi.json" || exit 1
-  else
-    starknet-compile-deprecated "$file" --output "$V0_ARTIFACT_PATH/$name.json" --abi "$V0_ARTIFACT_PATH/${name}Abi.json" || exit 1
-  fi
-done < <(find "$V0_CONTRACT_PATH" -name "*.cairo" -type f -print0)
-popd || exit 1
 echo "Done!"
 
 
