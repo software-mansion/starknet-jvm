f698ca3df4ce087b8e67c670dbdff849f585c044
Fiiranek <jellybean@onet.eu>
2024-04-29 13:01:19 +0200

    Merge `extractResult` and change its params

--- lib/src/main/kotlin/com/swmansion/starknet/provider/rpc/BuildJsonHttpDeserializer.kt
+++ lib/src/main/kotlin/com/swmansion/starknet/provider/rpc/BuildJsonHttpDeserializer.kt
@@ -3,7 +3,6 @@ package com.swmansion.starknet.provider.rpc
 import com.swmansion.starknet.data.serializers.JsonRpcErrorSerializer
 import com.swmansion.starknet.provider.exceptions.RequestFailedException
 import com.swmansion.starknet.provider.exceptions.RpcRequestFailedException
-import com.swmansion.starknet.service.http.HttpResponse
 import com.swmansion.starknet.service.http.HttpResponseDeserializer
 import kotlinx.serialization.*
 import kotlinx.serialization.json.*
@@ -11,87 +10,72 @@ import java.util.function.Function
 
 @Serializable
 private data class JsonRpcResponse<T>(
-    @SerialName("id")
-    val id: Int,
+        @SerialName("id")
+        val id: Int,
 
-    @SerialName("jsonrpc")
-    val jsonRpc: String,
+        @SerialName("jsonrpc")
+        val jsonRpc: String,
 
-    @SerialName("result")
-    val result: T? = null,
+        @SerialName("result")
+        val result: T? = null,
 
-    @SerialName("error")
-    val error: JsonRpcError? = null,
+        @SerialName("error")
+        val error: JsonRpcError? = null,
 )
 
 @Serializable(with = JsonRpcErrorSerializer::class)
 internal data class JsonRpcError(
-    @SerialName("code")
-    val code: Int,
+        @SerialName("code")
+        val code: Int,
 
-    @SerialName("message")
-    val message: String,
+        @SerialName("message")
+        val message: String,
 
-    @SerialName("data")
-    val data: String? = null,
+        @SerialName("data")
+        val data: String? = null,
 )
 
-private fun <T> extractResult(jsonRpcResponse: JsonRpcResponse<T>, response: HttpResponse): T {
+
+private fun <T> extractResult(jsonRpcResponse: JsonRpcResponse<T>, responsePayload: String, rpcResponsePayload: String = responsePayload): T {
     if (jsonRpcResponse.error != null) {
         throw RpcRequestFailedException(
-            code = jsonRpcResponse.error.code,
-            message = jsonRpcResponse.error.message,
-            data = jsonRpcResponse.error.data,
-            payload = response.body,
+                code = jsonRpcResponse.error.code,
+                message = jsonRpcResponse.error.message,
+                data = jsonRpcResponse.error.data,
+                payload = rpcResponsePayload,
         )
     }
 
     if (jsonRpcResponse.result == null) {
-        throw RequestFailedException(message = "Response did not contain a result", payload = response.body)
-    }
-    return jsonRpcResponse.result
-}
-
-private fun <T> extractResult(jsonRpcResponse: JsonRpcResponse<T>, response: HttpResponse, payload: String): T {
-    if (jsonRpcResponse.error != null) {
-        throw RpcRequestFailedException(
-            code = jsonRpcResponse.error.code,
-            message = jsonRpcResponse.error.message,
-            data = jsonRpcResponse.error.data,
-            payload = payload,
-        )
-    }
-
-    if (jsonRpcResponse.result == null) {
-        throw RequestFailedException(message = "Response did not contain a result", payload = response.body)
+        throw RequestFailedException(message = "Response did not contain a result", payload = responsePayload)
     }
     return jsonRpcResponse.result
 }
 
 @JvmSynthetic
 internal fun <T> buildJsonHttpDeserializer(
-    deserializationStrategy: KSerializer<T>,
-    deserializationJson: Json,
+        deserializationStrategy: KSerializer<T>,
+        deserializationJson: Json,
 ): HttpResponseDeserializer<T> {
     return Function { response ->
         if (!response.isSuccessful) {
             throw RequestFailedException(
-                payload = response.body,
+                    payload = response.body,
             )
         }
         val jsonRpcResponse =
-            deserializationJson.decodeFromString(
-                JsonRpcResponse.serializer(deserializationStrategy),
-                response.body,
-            )
+                deserializationJson.decodeFromString(
+                        JsonRpcResponse.serializer(deserializationStrategy),
+                        response.body,
+                )
 
-        extractResult(jsonRpcResponse, response)
+        extractResult(jsonRpcResponse, response.body)
     }
 }
 
 internal fun <T> buildJsonBatchHttpDeserializer(
-    deserializationStrategies: List<KSerializer<T>>,
-    deserializationJson: Json,
+        deserializationStrategies: List<KSerializer<T>>,
+        deserializationJson: Json,
 ): HttpResponseDeserializer<List<T>> {
     // TODO: In case of batch request, exception should not be thrown.
     // Instead, we want to return wrapped responses.
@@ -100,23 +84,23 @@ internal fun <T> buildJsonBatchHttpDeserializer(
     return Function { response ->
         if (!response.isSuccessful) {
             throw RequestFailedException(
-                payload = response.body,
+                    payload = response.body,
             )
         }
 
         val jsonArray = Json.parseToJsonElement(response.body).jsonArray
         val jsonRpcResponses = deserializationStrategies.mapIndexed { index, strategy ->
             deserializationJson.decodeFromString(
-                JsonRpcResponse.serializer(strategy),
-                jsonArray[index].toString(),
+                    JsonRpcResponse.serializer(strategy),
+                    jsonArray[index].toString(),
             )
         }
 
         val results = jsonRpcResponses.sortedBy { it.id }.zip(jsonArray)
-            .map { (jsonRpcResponse, json) ->
-                val payload = json.toString()
-                extractResult(jsonRpcResponse, response, payload)
-            }
+                .map { (jsonRpcResponse, json) ->
+                    val payload = json.toString()
+                    extractResult(jsonRpcResponse, response.body, payload)
+                }
         results
     }
 }
