357b8d13ad296f13344284b9d99328315a5a9cf7
Fiiranek <jellybean@onet.eu>
2024-05-06 12:22:52 +0200

    Import `toFelt` extension

--- lib/src/main/kotlin/com/swmansion/starknet/data/types/FeltArray.kt
+++ lib/src/main/kotlin/com/swmansion/starknet/data/types/FeltArray.kt
@@ -1,6 +1,7 @@
 package com.swmansion.starknet.data.types
 
 import com.swmansion.starknet.data.types.conversions.ConvertibleToCalldata
+import com.swmansion.starknet.extensions.toFelt
 
 data class FeltArray(private val list: MutableList<Felt>) : ConvertibleToCalldata, MutableList<Felt> by list {
     constructor(vararg elements: Felt) : this(elements.toMutableList())
