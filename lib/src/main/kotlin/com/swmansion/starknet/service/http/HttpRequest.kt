3ce561934ab846e6d0cb475e48965e42b7c89e2b
Franciszek Job <54181625+franciszekjob@users.noreply.github.com>
2024-05-09 00:06:27 +0200

    Update lib/src/main/kotlin/com/swmansion/starknet/service/http/HttpRequest.kt
    Co-authored-by: Maksim Zdobnikau <43750648+DelevoXDG@users.noreply.github.com>

--- lib/src/main/kotlin/com/swmansion/starknet/service/http/HttpRequest.kt
+++ lib/src/main/kotlin/com/swmansion/starknet/service/http/HttpRequest.kt
@@ -37,7 +37,6 @@ class HttpRequest<T> private constructor(
                 Json.encodeToString(jsonRpcRequest),
             )
         },
-
         deserializer = buildJsonHttpDeserializer(serializer, deserializationJson),
         service = service,
     )
