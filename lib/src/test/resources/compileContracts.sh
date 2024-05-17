93449e3f113430ff4b29c08280d2109fc6a17ffa
Fiiranek <jellybean@onet.eu>
2024-05-17 11:27:53 +0200

    Move `scarb` installation from shell script to `checks.yml`

--- lib/src/test/resources/compileContracts.sh
+++ lib/src/test/resources/compileContracts.sh
@@ -8,16 +8,10 @@ readonly V1_SCARB_VERSION="0.4.0"
 readonly V2_SCARB_VERSION="0.7.0"
 readonly V2_6_SCARB_VERSION="2.6.0"
 
-# TODO: (#365) Use asdf action to install scarb instead
-echo "Installing scarb..."
-asdf plugin add scarb || true
-echo "Done!"
-
 
 echo "Compiling v1 contracts.."
 pushd "$(dirname "$0")" || exit 1
 pushd "$V1_CONTRACT_PATH" || exit 1
-asdf install scarb $V1_SCARB_VERSION || true
 asdf local scarb $V1_SCARB_VERSION || exit 1
 scarb --profile release build
 popd || exit 1
@@ -28,7 +22,6 @@ echo "Done!"
 echo "Compiling v2 contracts.."
 pushd "$(dirname "$0")" || exit 1
 pushd "$V2_CONTRACT_PATH" || exit 1
-asdf install scarb $V2_SCARB_VERSION || true
 asdf local scarb $V2_SCARB_VERSION || exit 1
 scarb --profile release build
 popd || exit 1
@@ -38,7 +31,6 @@ echo "Done!"
 echo "Compiling v2.6 contracts.."
 pushd "$(dirname "$0")" || exit 1
 pushd "$V2_6_CONTRACT_PATH" || exit 1
-asdf install scarb $V2_6_SCARB_VERSION || true
 asdf local scarb $V2_6_SCARB_VERSION || exit 1
 scarb --profile release build
 popd || exit 1
