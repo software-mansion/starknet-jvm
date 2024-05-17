6e719ac5e97557ed5576df2059f542d950dda652
Fiiranek <jellybean@onet.eu>
2024-05-17 10:37:23 +0200

    Add missing `@JvmOverloads` to `FeltArray` constructor

--- lib/src/main/kotlin/com/swmansion/starknet/data/types/FeltArray.kt
+++ lib/src/main/kotlin/com/swmansion/starknet/data/types/FeltArray.kt
@@ -5,6 +5,8 @@ import com.swmansion.starknet.extensions.toFelt
 
 data class FeltArray(private val list: MutableList<Felt>) : ConvertibleToCalldata, MutableList<Felt> by list {
     constructor(vararg elements: Felt) : this(elements.toMutableList())
+
+    @JvmOverloads
     constructor(collection: Collection<Felt> = emptyList()) : this(collection.toMutableList())
 
     override fun toCalldata(): List<Felt> = listOf(size.toFelt) + list
