d8d8a84535bc0a4958c4492107acf4406eed70c9
Fiiranek <jellybean@onet.eu>
2024-05-22 10:26:57 +0200

    Restore initial approach of installing `scarb` in CI

--- lib/src/test/resources/compileContracts.sh
+++ lib/src/test/resources/compileContracts.sh
@@ -8,9 +8,15 @@ readonly V1_SCARB_VERSION="0.4.0"
 readonly V2_SCARB_VERSION="0.7.0"
 readonly V2_6_SCARB_VERSION="2.6.0"
 
+echo "Installing scarb..."
+asdf plugin add scarb || true
+echo "Done!"
+
+
 echo "Compiling v1 contracts.."
 pushd "$(dirname "$0")" || exit 1
 pushd "$V1_CONTRACT_PATH" || exit 1
+asdf install scarb $V1_SCARB_VERSION || true
 asdf local scarb $V1_SCARB_VERSION || exit 1
 scarb --profile release build
 popd || exit 1
@@ -21,6 +27,7 @@ echo "Done!"
 echo "Compiling v2 contracts.."
 pushd "$(dirname "$0")" || exit 1
 pushd "$V2_CONTRACT_PATH" || exit 1
+asdf install scarb $V2_SCARB_VERSION || true
 asdf local scarb $V2_SCARB_VERSION || exit 1
 scarb --profile release build
 popd || exit 1
@@ -30,6 +37,7 @@ echo "Done!"
 echo "Compiling v2.6 contracts.."
 pushd "$(dirname "$0")" || exit 1
 pushd "$V2_6_CONTRACT_PATH" || exit 1
+asdf install scarb $V2_6_SCARB_VERSION || true
 asdf local scarb $V2_6_SCARB_VERSION || exit 1
 scarb --profile release build
 popd || exit 1
