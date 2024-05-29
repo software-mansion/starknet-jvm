052826f71f6c6471b8eb390dddd0898f5819cade
Fiiranek <jellybean@onet.eu>
2024-05-29 11:18:56 +0200

    Add `BlockIdSerializerTest`; Add `ToDecimalTest`;

--- lib/src/main/kotlin/com/swmansion/starknet/data/serializers/BlockIdSerializer.kt
+++ lib/src/main/kotlin/com/swmansion/starknet/data/serializers/BlockIdSerializer.kt
@@ -19,10 +19,12 @@ internal object BlockIdSerializer : KSerializer<BlockId> {
         val value = decoder.decodeString()
 
         if (BlockTag.entries.map { it.tag }.contains(value)) {
-            val tag = BlockTag.valueOf(value)
+            val tag = BlockTag.entries.firstOrNull() { it.tag == value } ?: throw IllegalArgumentException("Unknown block tag: $value")
             return BlockId.Tag(tag)
         }
-
+        if (value.toIntOrNull() != null) {
+            return BlockId.Number(value.toInt())
+        }
         return BlockId.Hash(Felt.fromHex(value))
     }
 
