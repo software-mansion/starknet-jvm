1b7a6590812f7c7c3cd2ab6e4c5ac27128d07cfb
Fiiranek <jellybean@onet.eu>
2024-06-03 03:55:46 +0200

    Add tests for string and int response serializers

---
+++ lib/src/test/kotlin/com/swmansion/starknet/data/serializers/IntResponseSerializerTest.kt
@@ -0,0 +1,15 @@
+package com.swmansion.starknet.data.serializers
+
+import com.swmansion.starknet.data.types.IntResponse
+import kotlinx.serialization.json.Json
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class IntResponseSerializerTest {
+    @Test
+    fun `serialize response`() {
+        val expected = "123"
+        val encoded = Json.encodeToString(IntResponse.serializer(), IntResponse(123))
+        assertEquals(expected, encoded)
+    }
+}
---
+++ lib/src/test/kotlin/com/swmansion/starknet/data/serializers/StringResponseSerializerTest.kt
@@ -0,0 +1,15 @@
+package com.swmansion.starknet.data.serializers
+
+import com.swmansion.starknet.data.types.StringResponse
+import kotlinx.serialization.json.Json
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class StringResponseSerializerTest {
+    @Test
+    fun `serialize string response`() {
+        val expected = "\"Example text\""
+        val encoded = Json.encodeToString(StringResponse.serializer(), StringResponse("Example text"))
+        assertEquals(expected, encoded)
+    }
+}
