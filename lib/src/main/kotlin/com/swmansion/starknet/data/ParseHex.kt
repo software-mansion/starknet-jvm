696c1ff8feeb5a000403833f287e4a1c8e1b6ed6
Fiiranek <jellybean@onet.eu>
2024-06-10 17:39:08 +0200

    Remove unneeded zeros trimming

--- lib/src/main/kotlin/com/swmansion/starknet/data/ParseHex.kt
+++ lib/src/main/kotlin/com/swmansion/starknet/data/ParseHex.kt
@@ -9,10 +9,5 @@ internal fun parseHex(value: String): BigInteger {
     if (!value.startsWith("0x")) {
         throw IllegalArgumentException("Hex must start with 0x")
     }
-
-    val trimmedValue = value.removePrefix("0x").trimStart('0')
-    if (trimmedValue.isEmpty()) {
-        return BigInteger.ZERO
-    }
-    return BigInteger(trimmedValue, 16)
+    return BigInteger(value.removePrefix("0x"), 16)
 }
