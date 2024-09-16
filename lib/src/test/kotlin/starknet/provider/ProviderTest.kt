19b48fd4b56decde26158b4f8bd3ab2b7275d8c9
Fiiranek <jellybean@onet.eu>
2024-09-16 18:07:30 +0200

    Fix linting

--- lib/src/test/kotlin/starknet/provider/ProviderTest.kt
+++ lib/src/test/kotlin/starknet/provider/ProviderTest.kt
@@ -10,7 +10,6 @@ import com.swmansion.starknet.service.http.HttpService
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.*
 import starknet.utils.DevnetClient
