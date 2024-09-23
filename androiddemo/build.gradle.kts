5e7edd328eb13a71ff11fc039039671b76bc8835
Fiiranek <jellybean@onet.eu>
2024-09-23 15:08:22 +0200

    Clean up build gradle for android demo

--- androiddemo/build.gradle.kts
+++ androiddemo/build.gradle.kts
@@ -58,6 +58,3 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
-tasks.register<Wrapper>("wrapper") {
-    gradleVersion = "7.2"
-}
