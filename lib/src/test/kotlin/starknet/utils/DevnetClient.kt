4dbdea178011998a06112273c21d63f24bb93f02
Fiiranek <jellybean@onet.eu>
2025-03-20 16:07:21 +0100

    Increase devnet timeout to 8 seconds

--- lib/src/test/kotlin/starknet/utils/DevnetClient.kt
+++ lib/src/test/kotlin/starknet/utils/DevnetClient.kt
@@ -108,7 +108,7 @@ class DevnetClient(
             stateArchiveCapacity.value,
         )
         devnetProcess = devnetProcessBuilder.start()
-        devnetProcess.waitFor(3, TimeUnit.SECONDS)
+        devnetProcess.waitFor(8, TimeUnit.SECONDS)
 
         if (!devnetProcess.isAlive) {
             throw DevnetSetupFailedException("Could not start devnet process")
