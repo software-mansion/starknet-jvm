b40fa1eb1d75d0b67b391d905c77bd1ba180bbdc
Fiiranek <jellybean@onet.eu>
2025-07-27 09:14:46 +0200

    Add test for block with old txs

--- lib/src/test/kotlin/starknet/helpers/TipTest.kt
+++ lib/src/test/kotlin/starknet/helpers/TipTest.kt
@@ -8,7 +8,6 @@ import com.swmansion.starknet.service.http.HttpService
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.parallel.Execution
 import org.junit.jupiter.api.parallel.ExecutionMode
@@ -45,7 +44,7 @@ class TipTest {
     }
 
     @Test
-    fun noTransactionsInBlock() {
+    fun `block without transactions`() {
         val mockedResponse = """
             {
                 "id": 0,
@@ -87,7 +86,7 @@ class TipTest {
     }
 
     @Test
-    fun estimateTipOddTransactionsNumber() {
+    fun `odd transactions number`() {
         // 3 transactions, tips: 100, 200, 300
         val mockedResponse = """
             {
@@ -224,7 +223,7 @@ class TipTest {
     }
 
     @Test
-    fun estimateTipEvenTransactionsNumber() {
+    fun `even transactions number`() {
         // 4 transactions, tips: 100, 200, 300, 400
         val mockedResponse = """
             {
@@ -389,5 +388,156 @@ class TipTest {
         val tipEstimate = estimateTip(provider)
 
         assertEquals(Uint64(250), tipEstimate)
+    }
+
+    @Test
+    fun `block with old transactions`() {
+        // 3 transactions v3, tips: 100, 200, 300
+        // 1 transaction v1
+        val mockedResponse = """
+            {
+                "id": 0,
+                "jsonrpc": "2.0",
+                "result": {
+                    "block_hash": "0x1",
+                    "block_number": 111,
+                    "l1_da_mode": "BLOB",
+                    "l1_data_gas_price": {
+                        "price_in_fri": "0x1",
+                        "price_in_wei": "0x1"
+                    },
+                    "l1_gas_price": {
+                        "price_in_fri": "0x1",
+                        "price_in_wei": "0x1"
+                    },
+                    "l2_gas_price": {
+                        "price_in_fri": "0x1",
+                        "price_in_wei": "0x1"
+                    },
+                    "new_root": "0x1",
+                    "parent_hash": "0x1",
+                    "sequencer_address": "0x1",
+                    "starknet_version": "0.14.0",
+                    "status": "ACCEPTED_ON_L2",
+                    "timestamp": 123,
+                    "transactions": [
+                        {
+                            "calldata": [],
+                            "nonce": "0x1",
+                            "max_fee":  "0x123",
+                            "sender_address": "0x1",
+                            "signature": [
+                                "0x1",
+                                "0x1"
+                            ],
+                            "transaction_hash": "0x1",
+                            "type": "INVOKE",
+                            "version": "0x1"
+                        },
+                        {
+                            "account_deployment_data": [],
+                            "calldata": [],
+                            "fee_data_availability_mode": "L1",
+                            "nonce": "0x1",
+                            "nonce_data_availability_mode": "L1",
+                            "paymaster_data": [],
+                            "resource_bounds": {
+                                "l1_data_gas": {
+                                    "max_amount": "0x1",
+                                    "max_price_per_unit": "0x1"
+                                },
+                                "l1_gas": {
+                                    "max_amount": "0x1",
+                                    "max_price_per_unit": "0x1"
+                                },
+                                "l2_gas": {
+                                    "max_amount": "0x1",
+                                    "max_price_per_unit": "0x1"
+                                }
+                            },
+                            "sender_address": "0x1",
+                            "signature": [
+                                "0x1",
+                                "0x1"
+                            ],
+                            "tip": "0x64",
+                            "transaction_hash": "0x1",
+                            "type": "INVOKE",
+                            "version": "0x3"
+                        },
+                        {
+                            "account_deployment_data": [],
+                            "calldata": [],
+                            "fee_data_availability_mode": "L1",
+                            "nonce": "0x1",
+                            "nonce_data_availability_mode": "L1",
+                            "paymaster_data": [],
+                            "resource_bounds": {
+                                "l1_data_gas": {
+                                    "max_amount": "0x1",
+                                    "max_price_per_unit": "0x1"
+                                },
+                                "l1_gas": {
+                                    "max_amount": "0x1",
+                                    "max_price_per_unit": "0x1"
+                                },
+                                "l2_gas": {
+                                    "max_amount": "0x1",
+                                    "max_price_per_unit": "0x1"
+                                }
+                            },
+                            "sender_address": "0x1",
+                            "signature": [
+                                "0x1",
+                                "0x1"
+                            ],
+                            "tip": "0xc8",
+                            "transaction_hash": "0x1",
+                            "type": "INVOKE",
+                            "version": "0x3"
+                        },
+                        {
+                            "account_deployment_data": [],
+                            "calldata": [],
+                            "fee_data_availability_mode": "L1",
+                            "nonce": "0x1",
+                            "nonce_data_availability_mode": "L1",
+                            "paymaster_data": [],
+                            "resource_bounds": {
+                                "l1_data_gas": {
+                                    "max_amount": "0x1",
+                                    "max_price_per_unit": "0x1"
+                                },
+                                "l1_gas": {
+                                    "max_amount": "0x1",
+                                    "max_price_per_unit": "0x1"
+                                },
+                                "l2_gas": {
+                                    "max_amount": "0x1",
+                                    "max_price_per_unit": "0x1"
+                                }
+                            },
+                            "sender_address": "0x1",
+                            "signature": [
+                                "0x1",
+                                "0x1"
+                            ],
+                            "tip": "0x12c",
+                            "transaction_hash": "0x1",
+                            "type": "INVOKE",
+                            "version": "0x3"
+                        }
+                    ]
+                }
+            }
+        """.trimIndent()
+
+        val httpService = mock<HttpService> {
+            on { send(any()) } doReturn HttpResponse(true, 200, mockedResponse)
+        }
+        val provider = JsonRpcProvider(TipTest.rpcUrl, httpService)
+        val tipEstimate = estimateTip(provider)
+
+        assertEquals(Uint64(200), tipEstimate)
     }
 }
