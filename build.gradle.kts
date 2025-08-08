387394f7529fffa9f0fae12b60cf75e1da7b4154
Artur Michałek <52135326+cptartur@users.noreply.github.com>
2025-08-08 15:11:36 +0200

    Fix publish (#581)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,7 @@
+allprojects {
+    version = "0.16.0-rc.0"
+    group = "com.swmansion.starknet"
+}
 
 plugins {
     id("com.android.library") version "7.4.0" apply false
@@ -8,4 +12,14 @@ plugins {
     id("org.jetbrains.dokka") version "1.8.20" apply false
     id("org.jmailen.kotlinter") version "3.15.0"
     id("org.jetbrains.kotlinx.kover") version "0.7.2"
+    id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
+        }
+    }
 }
