e1b3b6abb43e9558579638247c48a5bd3d16e74c
Artur Michalek <artur.michalek@swmansion.com>
2025-11-21 12:56:23 +0100

    Rename `hashMethodFromRpcVersion` and make public

--- lib/src/test/kotlin/com/swmansion/starknet/helpers/RpcTest.kt
+++ lib/src/test/kotlin/com/swmansion/starknet/helpers/RpcTest.kt
@@ -8,23 +8,24 @@ import org.junit.jupiter.params.provider.MethodSource
 class RpcTest {
     companion object {
         @JvmStatic
-        private fun getTestCases(): List<Pair<Version, HashMethod>> {
+        private fun getTestCases(): List<Pair<String, HashMethod>> {
             return listOf(
-                Pair(Version.parse("0.9.0"), HashMethod.POSEIDON),
-                Pair(Version.parse("0.9.1"), HashMethod.POSEIDON),
-                Pair(Version.parse("0.10.0"), HashMethod.BLAKE2S),
-                Pair(Version.parse("0.10.0-rc.1"), HashMethod.BLAKE2S),
-                Pair(Version.parse("0.11.5"), HashMethod.BLAKE2S),
-                Pair(Version.parse("1.0.0"), HashMethod.BLAKE2S),
+                Pair("0.9.0", HashMethod.POSEIDON),
+                Pair("0.9.1", HashMethod.POSEIDON),
+                Pair("0.10.0", HashMethod.POSEIDON),
+                Pair("0.14.0", HashMethod.POSEIDON),
+                Pair("0.14.1-rc.0", HashMethod.BLAKE2S),
+                Pair("0.14.1", HashMethod.BLAKE2S),
+                Pair("1.0.0", HashMethod.BLAKE2S),
             )
         }
     }
 
     @ParameterizedTest
     @MethodSource("getTestCases")
-    fun `test hash method from RPC version`(testCase: Pair<Version, HashMethod>) {
+    fun `test hash method from RPC version`(testCase: Pair<String, HashMethod>) {
         val (version, expectedHashMethod) = testCase
-        val actualHashMethod = hashMethodFromRpcVersion(version)
+        val actualHashMethod = getHashMethodFromStarknetVersion(version)
         assert(actualHashMethod == expectedHashMethod) {
             "For version $version, expected hash method $expectedHashMethod but got $actualHashMethod"
         }
